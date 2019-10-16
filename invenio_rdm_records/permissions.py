429c9d757c7541751f3f671fa62b855df8182551
Pablo Panero <ppanero@cern.ch>
2019-10-16 10:37:22 +0200

    tests: fix docstrings and dependencies to pass tests

--- invenio_rdm_records/permissions.py
+++ invenio_rdm_records/permissions.py
@@ -4,8 +4,8 @@
 # Copyright (C) 2019 Northwestern University,
 #                    Galter Health Sciences Library & Learning Center.
 #
-# Invenio-RDM-Records is free software; you can redistribute it and/or modify it
-# under the terms of the MIT License; see LICENSE file for more details.
+# Invenio-RDM-Records is free software; you can redistribute it and/or modify
+# it under the terms of the MIT License; see LICENSE file for more details.
 
 """Permissions for Invenio App RDM."""
 
