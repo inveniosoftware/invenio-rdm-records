fc005f78181dde9b0e4300de569936bfa66abbc4
Pablo Panero <ppanero@cern.ch>
2019-10-21 10:51:47 +0200

    mappings: add elasticsearch v7 support

---
+++ invenio_rdm_records/mappings/v7/__init__.py
@@ -0,0 +1,10 @@
+# -*- coding: utf-8 -*-
+#
+# Copyright (C) 2019 CERN.
+# Copyright (C) 2019 Northwestern University,
+#                    Galter Health Sciences Library & Learning Center.
+#
+# Invenio-RDM-Records is free software; you can redistribute it and/or modify
+# it under the terms of the MIT License; see LICENSE file for more details.
+
+"""RDM DataCite based Elasticsearch version 7.x mappings."""
