eee236907d706b9dad0ac2f960468a37fa0b5d7d
Pablo Panero <ppanero@cern.ch>
2020-02-03 11:27:21 +0100

    marshmallow: inherit from BaseSchema and refactor validation

--- invenio_rdm_records/marshmallow/json.py
+++ invenio_rdm_records/marshmallow/json.py
@@ -12,9 +12,10 @@ from __future__ import absolute_import, print_function
 
 import arrow
 from flask_babelex import lazy_gettext as _
-from invenio_records_rest.schemas import Nested, StrictKeysMixin
+from invenio_records_rest.schemas import Nested
 from invenio_records_rest.schemas.fields import DateString, \
     PersistentIdentifier, SanitizedUnicode
+from invenio_rest.serializer import BaseSchema
 from marshmallow import ValidationError, fields, pre_load, validate, \
     validates, validates_schema
 
@@ -22,28 +23,28 @@ from ..models import ObjectType
 from .utils import api_link_for, validate_iso639_3
 
 
-class AccessConditionSchemaV1(StrictKeysMixin):
+class AccessConditionSchemaV1(BaseSchema):
     """Access condition."""
 
     condition = SanitizedUnicode()
     link_validity = SanitizedUnicode()
 
 
-class CommunitySchemaV1(StrictKeysMixin):
+class CommunitySchemaV1(BaseSchema):
     """Communities to which the record belongs to."""
 
     primary = SanitizedUnicode(required=True)
     secondary = fields.List(SanitizedUnicode())
 
 
-class IdentifierSchemaV1(StrictKeysMixin):
+class IdentifierSchemaV1(BaseSchema):
     """Extra/Alternate identifiers of the record."""
 
     identifier = SanitizedUnicode(required=True)
     scheme = SanitizedUnicode(required=True)
 
 
-class AffiliationSchemaV1(StrictKeysMixin):
+class AffiliationSchemaV1(BaseSchema):
     """Affiliation of a creator/contributor."""
 
     name = SanitizedUnicode(required=True)
@@ -51,7 +52,7 @@ class AffiliationSchemaV1(StrictKeysMixin):
     scheme = SanitizedUnicode(required=True)
 
 
-class CreatorSchemaV1(StrictKeysMixin):
+class CreatorSchemaV1(BaseSchema):
     """Creator schema."""
 
     NAMES = [
@@ -103,7 +104,7 @@ class ContributorSchemaV1(CreatorSchemaV1):
             ))
 
 
-class FilesSchemaV1(StrictKeysMixin):
+class FilesSchemaV1(BaseSchema):
     """Files metadata schema."""
 
     type = fields.String(required=True)
@@ -121,7 +122,7 @@ class FilesSchemaV1(StrictKeysMixin):
         }
 
 
-class InternalNoteSchemaV1(StrictKeysMixin):
+class InternalNoteSchemaV1(BaseSchema):
     """Internal notes shema."""
 
     user = SanitizedUnicode(required=True)
@@ -129,7 +130,7 @@ class InternalNoteSchemaV1(StrictKeysMixin):
     timestamp = DateString(required=True)
 
 
-class ResourceTypeSchemaV1(StrictKeysMixin):
+class ResourceTypeSchemaV1(BaseSchema):
     """Resource type schema."""
 
     type = fields.Str(
@@ -148,7 +149,7 @@ class ResourceTypeSchemaV1(StrictKeysMixin):
             raise ValidationError(_('Invalid resource type.'))
 
 
-class TitleSchemaV1(StrictKeysMixin):
+class TitleSchemaV1(BaseSchema):
     """Schema for the additional title."""
 
     TITLE_TYPES = [
@@ -166,7 +167,7 @@ class TitleSchemaV1(StrictKeysMixin):
     lang = SanitizedUnicode(validate=validate_iso639_3)
 
 
-class DescriptionSchemaV1(StrictKeysMixin):
+class DescriptionSchemaV1(BaseSchema):
     """Schema for the additional descriptions."""
 
     DESCRIPTION_TYPES = [
@@ -186,7 +187,7 @@ class DescriptionSchemaV1(StrictKeysMixin):
     lang = SanitizedUnicode(validate=validate_iso639_3)
 
 
-class LicenseSchemaV1(StrictKeysMixin):
+class LicenseSchemaV1(BaseSchema):
     """License schema."""
 
     license = SanitizedUnicode(required=True)
@@ -195,7 +196,7 @@ class LicenseSchemaV1(StrictKeysMixin):
     scheme = SanitizedUnicode()
 
 
-class SubjectSchemaV1(StrictKeysMixin):
+class SubjectSchemaV1(BaseSchema):
     """Subject schema."""
 
     subject = SanitizedUnicode(required=True)
@@ -203,7 +204,7 @@ class SubjectSchemaV1(StrictKeysMixin):
     scheme = SanitizedUnicode()
 
 
-class DateSchemaV1(StrictKeysMixin):
+class DateSchemaV1(BaseSchema):
     """Schema for date intervals."""
 
     DATE_TYPES = [
@@ -228,8 +229,27 @@ class DateSchemaV1(StrictKeysMixin):
         ))
     description = fields.Str()
 
+    @validates_schema
+    def validate_dates(self, data, **kwargs):
+        """Validate that start date is before the corresponding end date."""
+        start = arrow.get(data.get('start'), 'YYYY-MM-DD').date() \
+            if data.get('start') else None
+        end = arrow.get(data.get('end'), 'YYYY-MM-DD').date() \
+            if data.get('end') else None
 
-class RelatedIdentifierSchemaV1(StrictKeysMixin):
+        if not start and not end:
+            raise ValidationError(
+                _('There must be at least one date.'),
+                field_names=['dates']
+            )
+        if start and end and start > end:
+            raise ValidationError(
+                _('"start" date must be before "end" date.'),
+                field_names=['dates']
+            )
+
+
+class RelatedIdentifierSchemaV1(BaseSchema):
     """Related identifier schema."""
 
     RELATIONS = [
@@ -303,7 +323,7 @@ class RelatedIdentifierSchemaV1(StrictKeysMixin):
     resource_type = fields.Nested(ResourceTypeSchemaV1)
 
 
-class ReferenceSchemaV1(StrictKeysMixin):
+class ReferenceSchemaV1(BaseSchema):
     """Reference schema."""
 
     SCHEMES = [
@@ -320,14 +340,14 @@ class ReferenceSchemaV1(StrictKeysMixin):
         ))
 
 
-class PointSchemaV1(StrictKeysMixin):
+class PointSchemaV1(BaseSchema):
     """Point schema."""
 
     lat = fields.Number(required=True)
     lon = fields.Number(required=True)
 
 
-class LocationSchemaV1(StrictKeysMixin):
+class LocationSchemaV1(BaseSchema):
     """Location schema."""
 
     point = fields.Nested(PointSchemaV1)
@@ -335,26 +355,28 @@ class LocationSchemaV1(StrictKeysMixin):
     description = SanitizedUnicode()
 
 
-class MetadataSchemaV1(StrictKeysMixin):
+class MetadataSchemaV1(BaseSchema):
     """Schema for the record metadata."""
 
     # Administrative fields
     _visibility = fields.Bool(default=True)
     _visibility_files = fields.Bool(default=True)
-    _embargo_date = DateString()
-    _access_condition = fields.Nested(AccessConditionSchemaV1)
-    _contact = SanitizedUnicode()
-    _community = fields.Nested(CommunitySchemaV1)
-    _owners = fields.List(fields.Integer(), validate=validate.Length(min=1),
-                          dump_only=True, required=True)
-    _created_by = DateString()
+    # _access_condition = fields.Nested(AccessConditionSchemaV1)
+    _owners = fields.List(fields.Integer, validate=validate.Length(min=1),
+                          required=True)
+    _created_by = fields.Integer()
     _files = fields.List(fields.Nested(FilesSchemaV1(), dump_only=True))
     _default_preview = SanitizedUnicode()
     _internal_notes = fields.List(fields.Nested(InternalNoteSchemaV1))
+    # Extra non-administrative fields
+    _embargo_date = DateString(data_key="embargo_date")
+    _community = fields.Nested(CommunitySchemaV1, data_key="community")
+    _contact = SanitizedUnicode(data_key="contact")
     # Metadata fields
     resource_type = fields.Nested(ResourceTypeSchemaV1)
-    identifiers = fields.Nested(IdentifierSchemaV1)
-    creators = fields.List(Nested(CreatorSchemaV1, required=True))
+    identifiers = fields.List(fields.Nested(IdentifierSchemaV1))
+    creators = fields.List(Nested(CreatorSchemaV1))
+    contributors = fields.List(Nested(ContributorSchemaV1))
     titles = fields.List(fields.Nested(TitleSchemaV1))
     descriptions = fields.List(fields.Nested(DescriptionSchemaV1))
     publication_date = DateString()
@@ -368,26 +390,6 @@ class MetadataSchemaV1(StrictKeysMixin):
     references = fields.List(fields.Nested(ReferenceSchemaV1))
     locations = fields.List(fields.Nested(LocationSchemaV1))
 
-    @validates('dates')
-    def validate_dates(self, value):
-        """Validate that start date is before the corresponding end date."""
-        for interval in value:
-            start = arrow.get(interval.get('start'), 'YYYY-MM-DD').date() \
-                if interval.get('start') else None
-            end = arrow.get(interval.get('end'), 'YYYY-MM-DD').date() \
-                if interval.get('end') else None
-
-            if not start and not end:
-                raise ValidationError(
-                    _('There must be at least one date.'),
-                    field_names=['dates']
-                )
-            if start and end and start > end:
-                raise ValidationError(
-                    _('"start" date must be before "end" date.'),
-                    field_names=['dates']
-                )
-
     @validates('_embargo_date')
     def validate_embargo_date(self, value):
         """Validate that embargo date is in the future."""
@@ -397,8 +399,16 @@ class MetadataSchemaV1(StrictKeysMixin):
                 field_names=['embargo_date']
             )
 
+    @pre_load()
+    def preload_publicationdate(self, data, **kwargs):
+        """Default publication date."""
+        if 'publication_date' not in data:
+            data['publication_date'] = arrow.utcnow().date().isoformat()
 
-class RecordSchemaV1(StrictKeysMixin):
+        return data
+
+
+class RecordSchemaV1(BaseSchema):
     """Record schema."""
 
     # TODO: Use `RecordMetadataSchemaJSONV1` to inject PID in PUT/PATCH/...
