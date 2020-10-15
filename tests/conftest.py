5f910d1e4fa7c544f4c3dc81ac731eaec461522a
Lars Holm Nielsen <lars.holm.nielsen@cern.ch>
2020-10-15 15:08:12 +0200

    tests: typo in license header

--- tests/conftest.py
+++ tests/conftest.py
@@ -3,7 +3,7 @@
 # Copyright (C) 2019 CERN.
 # Copyright (C) 2019 Northwestern University.
 #
-# Invenio-RDM-Records is free software; you can redistribute it and/or modifya
+# Invenio-RDM-Records is free software; you can redistribute it and/or modify
 # it under the terms of the MIT License; see LICENSE file for more details.
 
 """Pytest configuration.
