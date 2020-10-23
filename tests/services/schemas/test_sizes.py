c9236414db4283f85cab6b64dc4c303a91f8dac8
Pablo Panero <pablo.panero@cern.ch>
2020-10-23 10:44:19 +0200

    schema: add sizes

---
+++ tests/services/schemas/test_sizes.py
@@ -0,0 +1,30 @@
+# -*- coding: utf-8 -*-
+#
+# Copyright (C) 2020 CERN.
+#
+# Invenio-RDM-Records is free software; you can redistribute it and/or modify
+# it under the terms of the MIT License; see LICENSE file for more details.
+
+"""Test sizes schema."""
+
+import pytest
+from marshmallow import ValidationError
+
+from invenio_rdm_records.services.schemas.metadata import MetadataSchema
+
+
+@pytest.mark.parametrize("size", [([]), (["100 pages"])])
+def test_valid_size(size, app, minimal_record, vocabulary_clear):
+    metadata = minimal_record['metadata']
+    metadata['sizes'] = size
+    data = MetadataSchema().load(metadata)
+    assert data['sizes'] == metadata['sizes']
+
+
+@pytest.mark.parametrize("size", [([100]), ("11 pages"), ([""])])
+def test_invalid_size(size, app, minimal_record, vocabulary_clear):
+    metadata = minimal_record['metadata']
+    metadata['sizes'] = size
+
+    with pytest.raises(ValidationError):
+        data = MetadataSchema().load(metadata)
