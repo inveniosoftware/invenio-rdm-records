424cdc096411b08d2a862ef5f101e3792c6c7686
Pablo Panero <pablo.panero@cern.ch>
2020-10-23 10:50:50 +0200

    schema: add formats

---
+++ tests/services/schemas/test_formats.py
@@ -0,0 +1,30 @@
+# -*- coding: utf-8 -*-
+#
+# Copyright (C) 2020 CERN.
+#
+# Invenio-RDM-Records is free software; you can redistribute it and/or modify
+# it under the terms of the MIT License; see LICENSE file for more details.
+
+"""Test formats schema."""
+
+import pytest
+from marshmallow import ValidationError
+
+from invenio_rdm_records.services.schemas.metadata import MetadataSchema
+
+
+@pytest.mark.parametrize("format", [([]), (["application/pdf"])])
+def test_valid_size(format, app, minimal_record, vocabulary_clear):
+    metadata = minimal_record['metadata']
+    metadata['formats'] = format
+    data = MetadataSchema().load(metadata)
+    assert data['formats'] == metadata['formats']
+
+
+@pytest.mark.parametrize("format", [([100]), ("jpeg"), ([""])])
+def test_invalid_size(format, app, minimal_record, vocabulary_clear):
+    metadata = minimal_record['metadata']
+    metadata['formats'] = format
+
+    with pytest.raises(ValidationError):
+        data = MetadataSchema().load(metadata)
