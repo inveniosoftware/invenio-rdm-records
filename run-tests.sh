ea449ed27840a0b85d81263f5f7c409c0268416b
Pablo Panero <pablo.panero@cern.ch>
2020-10-28 09:26:52 +0100

    workflows: add test action

--- run-tests.sh
+++ run-tests.sh
@@ -7,10 +7,41 @@
 # Invenio-RDM-Records is free software; you can redistribute it and/or modify it
 # under the terms of the MIT License; see LICENSE file for more details.
 
-python -m check_manifest --ignore ".travis-*" && \
-python -m sphinx.cmd.build -qnNW docs docs/_build/html && \
-docker-services-cli up es postgresql redis
-python -m pytest
-tests_exit_code=$?
-docker-services-cli down
-exit "$tests_exit_code"
+# # Quit on errors
+# set -o errexit
+
+# # Quit on unbound symbols
+# set -o nounset
+
+check_manifest () {
+    python -m check_manifest --ignore ".*-requirements.txt"
+}
+
+check_sphinx () {
+  python -m sphinx.cmd.build -qnNW docs docs/_build/html
+}
+
+check_pytest () {
+  docker-services-cli up es postgresql redis
+  python -m pytest
+  tests_exit_code=$?
+  docker-services-cli down
+  exit "$tests_exit_code"
+}
+
+
+if [ $# -eq 0 ]
+then
+  check_manifest
+  check_sphinx
+  check_pytest
+else
+  for arg in "$@"
+  do
+      case $arg in
+          --check-manifest) check_manifest;;
+          --check-sphinx) check_sphinx;;
+          --check-pytest) check_pytest;;
+      esac
+  done
+fi
