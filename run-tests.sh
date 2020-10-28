fe79d34f96e769000af81d7f9b1dce55dd18ff22
Pablo Panero <pablo.panero@cern.ch>
2020-10-28 09:26:52 +0100

    workflows: add python tests github action

--- run-tests.sh
+++ run-tests.sh
@@ -7,9 +7,15 @@
 # Invenio-RDM-Records is free software; you can redistribute it and/or modify it
 # under the terms of the MIT License; see LICENSE file for more details.
 
-python -m check_manifest --ignore ".travis-*" && \
-python -m sphinx.cmd.build -qnNW docs docs/_build/html && \
-docker-services-cli up es postgresql redis
+# Quit on errors
+set -o errexit
+
+# Quit on unbound symbols
+set -o nounset
+
+python -m check_manifest --ignore ".*-requirements.txt"
+python -m sphinx.cmd.build -qnNW docs docs/_build/html
+docker-services-cli --verbose up es postgresql redis
 python -m pytest
 tests_exit_code=$?
 docker-services-cli down
