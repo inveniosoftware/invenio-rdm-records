abc39f09e7d7b73e8c61c3e501efe2e5e9f96cda
Alex Dutton <alex@cottagelabs.com>
2020-11-26 11:38:03 +0000

    Add ES Dumper for locations
    This infers a centroid from a geometry as best it can:
    
    * If the geometry is a point, we can just copy its coordinates
    * If shapely is installed we can use that to calculate a centroid
    * Otherwise, warn that shapely isn't installed, and do nothing

---
+++ invenio_rdm_records/records/dumpers/locations.py
@@ -0,0 +1,56 @@
+# -*- coding: utf-8 -*-
+#
+# Copyright (C) 2020 CERN.
+#
+# Invenio-RDM-Records is free software; you can redistribute it and/or modify
+# it under the terms of the MIT License; see LICENSE file for more details.
+
+"""DataCite-based data model for Invenio."""
+
+import warnings
+
+from invenio_records.dumpers import ElasticsearchDumperExt
+
+try:
+    import shapely.geometry
+except ImportError:
+    shapely = None
+
+
+class LocationsDumper(ElasticsearchDumperExt):
+    """ElasticSearch dumper for location data.
+
+    This does a few things:
+
+    * Removes/adds FeatureCollection and Feature types, as those are
+      constant
+    * Calculates a centroid for each location if a geometry is provided
+
+    Centroid calculation requires shapely, except for the simple case where
+    the geometry is a point.
+    """
+
+    def dump(self, record, data):
+        """Dump the data."""
+        if 'locations' not in data:
+            return data
+        for feature in data['locations']['features']:
+            geometry = feature.get('geometry')
+            if geometry:
+                if geometry['type'] == 'Point':
+                    feature['centroid'] = geometry['coordinates']
+                elif shapely:
+                    centroid = shapely.geometry.shape(geometry).centroid
+                    feature['centroid'] = [centroid.x, centroid.y]
+                else:
+                    warnings.warn(
+                        "Trying to find centroid for non-point geometry, but "
+                        "shapely isn't installed."
+                    )
+
+    def load(self, data, record_cls):
+        """Load the data."""
+        if 'locations' not in data:
+            return
+        for feature in data['locations']['features']:
+            feature.pop('centroid', None)
