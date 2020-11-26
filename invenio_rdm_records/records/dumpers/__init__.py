1ac865c722dcb5bc4cf228fa0c5df96f15e9c254
Alex Dutton <alex@cottagelabs.com>
2020-11-26 11:35:35 +0000

    invenio_rdm_records.records.dumpers becomes a package
    This means we can put each dumper in its own module, for the sake of clarity.

---
+++ invenio_rdm_records/records/dumpers/__init__.py
@@ -0,0 +1,10 @@
+# -*- coding: utf-8 -*-
+#
+# Copyright (C) 2020 CERN.
+#
+# Invenio-RDM-Records is free software; you can redistribute it and/or modify
+# it under the terms of the MIT License; see LICENSE file for more details.
+
+"""ElasticSearch dumpers, for transforming to and from versions to index."""
+
+from .edtf import EDTFDumperExt
