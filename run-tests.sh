e24737c5303cefb91acfbd99e1ac79d7a5739d50
Florian Cassayre <florian.cassayre@gmail.com>
2020-12-08 16:25:07 +0100

    tests: adjust docker-services-cli env

--- run-tests.sh
+++ run-tests.sh
@@ -1,4 +1,4 @@
-#!/usr/bin/env sh
+#!/usr/bin/env bash
 # -*- coding: utf-8 -*-
 #
 # Copyright (C) 2019-2020 CERN.
@@ -7,16 +7,24 @@
 # Invenio-RDM-Records is free software; you can redistribute it and/or modify it
 # under the terms of the MIT License; see LICENSE file for more details.
 
+# Usage:
+#   env DB=postgresql ./run-tests.sh
+
 # Quit on errors
 set -o errexit
 
 # Quit on unbound symbols
 set -o nounset
 
+# Always bring down services
+function cleanup {
+  eval "$(docker-services-cli down --env)"
+}
+trap cleanup EXIT
+
 python -m check_manifest --ignore ".*-requirements.txt"
 python -m sphinx.cmd.build -qnNW docs docs/_build/html
-docker-services-cli --verbose up es postgresql redis
+eval "$(docker-services-cli up --db ${DB:-postgresql} --search ${SEARCH:-elasticsearch} --mq redis --env)"
 python -m pytest
 tests_exit_code=$?
-docker-services-cli down
 exit "$tests_exit_code"
