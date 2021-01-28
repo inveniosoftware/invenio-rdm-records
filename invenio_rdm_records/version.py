ee94dfa629b56d3af19ca8afb87b0c69c7863eaa
Guillaume Viger <fenekku@fenekku.com>
2021-01-28 17:31:48 -0600

    release: v0.26.3

--- invenio_rdm_records/version.py
+++ invenio_rdm_records/version.py
@@ -1,7 +1,7 @@
 # -*- coding: utf-8 -*-
 #
-# Copyright (C) 2019-2020 CERN.
-# Copyright (C) 2019-2020 Northwestern University.
+# Copyright (C) 2019-2021 CERN.
+# Copyright (C) 2019-2021 Northwestern University.
 #
 # Invenio-RDM-Records is free software; you can redistribute it and/or modify
 # it under the terms of the MIT License; see LICENSE file for more details.
@@ -12,4 +12,4 @@ This file is imported by ``invenio_rdm_records.__init__``,
 and parsed by ``setup.py``.
 """
 
-__version__ = '0.26.2'
+__version__ = '0.26.3'
