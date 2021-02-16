21923901050c2da1a5956ef14b022c15c3060b4c
Lars Holm Nielsen <lars.holm.nielsen@cern.ch>
2021-02-16 23:01:14 +0100

    installation: bump invenio-vocabularies

--- setup.py
+++ setup.py
@@ -65,8 +65,8 @@ install_requires = [
     'arrow>=0.17.0',
     'Faker>=2.0.3',
     'ftfy>=4.4.3,<5.0.0',
-    'invenio-drafts-resources>=0.9.0,<0.10.0',
-    'invenio-vocabularies>=0.3.1,<0.4.0',
+    'invenio-drafts-resources>=0.9.1,<0.10.0',
+    'invenio-vocabularies>=0.3.2,<0.4.0',
     'pytz>=2020.4',
     'pyyaml>=5.4.0',
 ]
