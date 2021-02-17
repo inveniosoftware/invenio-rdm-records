052ad6a2dbede3b5542c94164453a3483f97eb12
Maximilian Moser <maximilian.moser@tuwien.ac.at>
2021-02-17 15:04:11 +0100

    tests: add test case for updating a record via dict

--- tests/services/components/test_access_component.py
+++ tests/services/components/test_access_component.py
@@ -7,6 +7,9 @@
 
 """Tests for the service AccessComponent."""
 
+from datetime import timedelta
+
+import arrow
 import pytest
 from flask_principal import Identity, UserNeed
 from marshmallow import ValidationError
@@ -72,3 +75,50 @@ def test_access_component_set_owner(minimal_record, users):
 
     assert len(record.access.owners) == 1
     assert list(record.access.owners)[0].resolve() == user
+
+
+def test_access_component_update_access_via_json(
+    minimal_record, users, identity_simple
+):
+    next_year = arrow.utcnow().datetime + timedelta(days=+365)
+    restricted_access = {
+        "owned_by": [{"user": users[0].id}],
+        "grants": [],
+        "record": "restricted",
+        "files": "restricted",
+        "embargo": {
+            "until": next_year.strftime("%Y-%m-%d"),
+            "active": True,
+            "reason": "nothing in particular",
+        },
+    }
+    minimal_record["access"] = restricted_access
+
+    # create an initially restricted-access record
+    record = RDMRecord.create(minimal_record)
+    component = AccessComponent(RDMRecordService())
+    component.create(identity_simple, minimal_record, record)
+
+    prot = record.access.protection
+    assert record.access.embargo is not None
+    assert "embargo" in record["access"]
+    assert prot.record == record["access"]["record"] == "restricted"
+    assert prot.files == record["access"]["files"] == "restricted"
+
+    # update the record's access via new json data
+    # (instead of via the record's access object)
+    new_data = minimal_record.copy()
+    public_access = {
+        "owned_by": [{"user": users[0].id}],
+        "grants": [],
+        "record": "public",
+        "files": "public",
+    }
+    new_data["access"] = public_access
+    component.create(identity_simple, new_data, record)
+
+    prot = record.access.protection
+    assert not record.access.embargo
+    assert "embargo" not in record["access"]
+    assert prot.record == record["access"]["record"] == "public"
+    assert prot.files == record["access"]["files"] == "public"
