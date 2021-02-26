f23bb8cfdc20cb5f6be4c171dabdff261faddb10
Lars Holm Nielsen <lars.holm.nielsen@cern.ch>
2021-02-26 10:11:46 +0100

    fixtures: update language fixture with full iso

--- invenio_rdm_records/fixtures/data/vocabularies/languages.py
+++ invenio_rdm_records/fixtures/data/vocabularies/languages.py
@@ -12,35 +12,42 @@ from os.path import dirname, join
 
 import pycountry
 import yaml
-from babel import Locale, UnknownLocaleError
-from invenio_i18n.ext import current_i18n
+
+scope_map = {
+    "I": "individual",
+    "M": "macrolanguage",
+    "S": "special-scope",
+}
+
+type_map = {
+    "A": "ancient",
+    "C": "constructed",
+    "E": "extinct",
+    "H": "historical",
+    "L": "living",
+    "S": "special-type",
+}
+
+
+def iter_languages():
+    """Iterate over languages."""
+    for lang in pycountry.languages:
+        record = {
+            "id": lang.alpha_3,
+            "title": {
+                "en": lang.name
+            },
+            "tags": [scope_map[lang.scope], type_map[lang.type]],
+        }
+        yield record
 
 
 def create_yaml_languages():
     """Create languages to yaml file."""
-    records = []
-    instance_languages = [lang[0] for lang in current_i18n.get_languages()]
-
-    for pycountry_language in pycountry.languages:
-        pycountry_language_code = pycountry_language.alpha_3
-        try:
-            # Parse the locale for the pycountry language entry
-            locale = Locale.parse(pycountry_language_code)
-            # Create the title dict for all the configured instance languages
-            title = {}
-            for instance_lang in instance_languages:
-                title[instance_lang] = locale.get_display_name(instance_lang)
-            # Create record
-            metadata = {
-                "id": pycountry_language_code,
-                "title": title,
-            }
-            records.append(metadata)
-        except UnknownLocaleError:
-            # Pass for the pycountry languages
-            # that babel cannot parse the locale
-            pass
     with open(join(dirname(__file__), 'languages.yaml'), 'w') as f:
-        data = yaml.dump(records, f)
+        yaml.dump(list(iter_languages()), f)
 
-    return data
+
+if __name__ == "__main__":
+    print("Creating languages.yaml...")
+    create_yaml_languages()
