f63baeadc17c63e120cd28a1e2b5f896e45102df
Maximilian Moser <maximilian.moser@tuwien.ac.at>
2021-03-11 17:21:51 +0100

    tests: add test cases for the secret links

---
+++ tests/links/test_sharing.py
@@ -0,0 +1,57 @@
+# -*- coding: utf-8 -*-
+#
+# Copyright (C) 2021 TU Wien.
+#
+# Invenio is free software; you can redistribute it and/or modify it
+# under the terms of the MIT License; see LICENSE file for more details.
+
+"""Test sharing of restricted records via secret links."""
+
+from invenio_access.permissions import authenticated_user
+from invenio_db import db
+
+from invenio_rdm_records.proxies import current_rdm_records
+from invenio_rdm_records.records import RDMRecord
+
+
+def test_read_restricted_record_with_secret_link(
+    app, client, location, minimal_record, es_clear, identity_simple
+):
+    """Test access to a restricted record via a shared link."""
+    identity_simple.provides.add(authenticated_user)
+    service = current_rdm_records.records_service
+    record_data = minimal_record.copy()
+    record_data["access"]["files"] = "restricted"
+    record_data["access"]["record"] = "restricted"
+
+    draft = service.create(identity=identity_simple, data=record_data)
+    record = service.publish(id_=draft.id, identity=identity_simple)
+    recid = record.id
+
+    link = record._record.access.links.create(
+        permission_level="read",
+    )
+
+    # FIXME without this, commit() won't work (b/c of jsonschema)
+    record._record.pop("status", None)
+    record._record.commit()
+    db.session.commit()
+
+    # the record shouldn't be accessible without login and/or token
+    response = client.get(f"/records/{recid}")
+    assert response.status_code == 403
+
+    # but it should be accessible with the token
+    response = client.get(
+        f"/records/{recid}",
+        query_string={"token": link.token},
+    )
+    assert response.status_code == 200
+
+    # the record shouldn't be showing up in search results, however
+    RDMRecord.index.refresh()
+    res = client.get(
+        "/records", query_string={"q": f"id:{recid}"}
+    )
+    assert res.status_code == 200
+    assert res.json["hits"]["total"] == 0
---
+++ tests/links/test_token_serializers.py
@@ -0,0 +1,119 @@
+# -*- coding: utf-8 -*-
+#
+# Copyright (C) 2021 TU Wien.
+#
+# Invenio is free software; you can redistribute it and/or modify it
+# under the terms of the MIT License; see LICENSE file for more details.
+
+"""Test token serializers."""
+
+from datetime import datetime, timedelta
+
+import pytest
+from itsdangerous import SignatureExpired
+
+from invenio_rdm_records.links.serializers import SecretLinkSerializer, \
+    TimedSecretLinkSerializer
+
+
+def test_serializer(base_app):
+    """Test the SecretLinkSerializer."""
+    with base_app.app_context():
+        serializer = SecretLinkSerializer()
+        tkn_str = serializer.create_token(
+            obj_id="someid", extra_data={"hello": "world"}
+        )
+        tkn_str2 = serializer.create_token(
+            obj_id="someid", extra_data={"hello": "world"}
+        )
+
+        # check the creation of tokens from the same data doesn't
+        # result in identical results
+        assert tkn_str != tkn_str2
+        assert isinstance(tkn_str, str)
+
+        # test token loading
+        tkn_dict = serializer.load_token(tkn_str)
+        assert tkn_dict["id"] == "someid"
+        assert tkn_dict["data"]["hello"] == "world"
+        assert "random" not in tkn_dict
+
+        # test the raw 'loads(...)'
+        tkn_dict_orig = serializer.loads(tkn_str)
+        assert "random" in tkn_dict_orig
+        del tkn_dict_orig["random"]
+        assert tkn_dict == tkn_dict_orig
+
+
+def test_serializer_validate_token(base_app):
+    """Test validation of a valid token."""
+    with base_app.app_context():
+        serializer = SecretLinkSerializer()
+        tkn_str = serializer.create_token(
+            obj_id="someid", extra_data={"hello": "world"}
+        )
+
+        tkn_dict = serializer.validate_token(
+            tkn_str, expected_data={"hello": "world"}
+        )
+        assert tkn_dict
+
+
+def test_serializer_validate_invalid_token(base_app):
+    """Test validation of an invalid token."""
+    with base_app.app_context():
+        serializer = SecretLinkSerializer()
+        tkn_dict = serializer.validate_token("asdf")
+        assert tkn_dict is None
+
+
+def test_timed_serializer(base_app):
+    """Test loading operations on valid tokens."""
+    with base_app.app_context():
+        in_10_mins = datetime.utcnow() + timedelta(minutes=10)
+        serializer = TimedSecretLinkSerializer(expires_at=in_10_mins)
+        tkn_str = serializer.create_token(
+            obj_id="someid", extra_data={"hello": "world"}
+        )
+        tkn_str2 = serializer.create_token(
+            obj_id="someid", extra_data={"hello": "world"}
+        )
+
+        # check the creation of tokens from the same data doesn't
+        # result in identical results
+        assert tkn_str != tkn_str2
+        assert isinstance(tkn_str, str)
+
+        # test token loading
+        tkn_dict = serializer.load_token(tkn_str)
+        assert tkn_dict["id"] == "someid"
+        assert tkn_dict["data"]["hello"] == "world"
+        assert "random" not in tkn_dict
+
+        # test the raw 'loads(...)'
+        tkn_dict_orig = serializer.loads(tkn_str)
+        assert "random" in tkn_dict_orig
+        del tkn_dict_orig["random"]
+        assert tkn_dict == tkn_dict_orig
+
+
+def test_timed_serializer_expired(base_app):
+    """Test loading operations on expired tokens."""
+    with base_app.app_context():
+        _10_mins_ago = datetime.utcnow() - timedelta(minutes=10)
+        serializer = TimedSecretLinkSerializer(expires_at=_10_mins_ago)
+        tkn_str = serializer.create_token(
+            obj_id="someid", extra_data={"hello": "world"}
+        )
+
+        # test token loading
+        with pytest.raises(SignatureExpired):
+            serializer.load_token(tkn_str)
+
+        # test the raw 'loads(...)'
+        with pytest.raises(SignatureExpired):
+            serializer.loads(tkn_str)
+
+        tkn_dict = serializer.load_token(tkn_str, force=True)
+        assert tkn_dict["id"] == "someid"
+        assert tkn_dict["data"] == {"hello": "world"}
