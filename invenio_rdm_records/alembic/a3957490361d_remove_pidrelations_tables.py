b205de0a94cfb2e6ca082f4fb47b10e93cff9edb
Maximilian Moser <maximilian.moser@tuwien.ac.at>
2021-03-15 11:00:08 +0100

    alembic: add recipe to remove PIDRelations tables if they exist

---
+++ invenio_rdm_records/alembic/a3957490361d_remove_pidrelations_tables.py
@@ -0,0 +1,33 @@
+#
+# This file is part of Invenio.
+# Copyright (C) 2021 TU Wien.
+#
+# Invenio is free software; you can redistribute it and/or modify it
+# under the terms of the MIT License; see LICENSE file for more details.
+
+"""Remove PIDRelations tables."""
+
+import sqlalchemy as sa
+from alembic import op
+from sqlalchemy.dialects import postgresql
+from sqlalchemy.engine.reflection import Inspector
+
+# revision identifiers, used by Alembic.
+revision = "a3957490361d"
+down_revision = "88d1463de5c0"
+branch_labels = ()
+depends_on = None
+
+
+def upgrade():
+    """Upgrade database."""
+    try:
+        # The table is not used and has no data.
+        op.drop_table("pidrelations_pidrelation")
+    except Exception:
+        pass
+
+
+def downgrade():
+    """Downgrade database."""
+    # no turning back
