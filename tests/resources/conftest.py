6d256440f31577c535cb75f4e2b0fe3aff34874d
Pablo Panero <pablo.panero@cern.ch>
2021-04-20 15:27:56 +0200

    pids: datacite serialization and tests
    Co-authored-by: Tom Morrell <tmorrell@caltech.edu>

--- tests/resources/conftest.py
+++ tests/resources/conftest.py
@@ -13,6 +13,8 @@ See https://pytest-invenio.readthedocs.io/ for documentation on which test
 fixtures are available.
 """
 
+from copy import deepcopy
+
 import pytest
 from invenio_app.factory import create_api
 
@@ -52,197 +54,6 @@ def app_config(app_config):
     return app_config
 
 
-@pytest.fixture(scope='function')
-def full_record():
-    """Full record dereferenced data, as is expeceted by the UI serializer."""
-    return {
-        "pids": {
-            "doi": {
-                "identifier": "10.5281/zenodo.1234",
-                "provider": "datacite",
-                "client": "zenodo"
-            },
-            "concept-doi": {
-                "identifier": "10.5281/zenodo.1234",
-                "provider": "datacite",
-                "client": "zenodo"
-            },
-            "handle": {
-                "identifier": "9.12314",
-                "provider": "cern-handle",
-                "client": "zenodo"
-            },
-            "oai": {
-                "identifier": "oai:zenodo.org:12345",
-                "provider": "zenodo"
-            }
-        },
-        "metadata": {
-            "resource_type": {
-                "type": "publication",
-                "subtype": "publication-article"
-            },
-            "creators": [{
-                "name": "Nielsen, Lars Holm",
-                "type": "personal",
-                "given_name": "Lars Holm",
-                "family_name": "Nielsen",
-                "identifiers": {
-                    "orcid": "0000-0001-8135-3489"
-                },
-                "affiliations": [{
-                    "name": "CERN",
-                    "identifiers": {
-                        "ror": "01ggx4157",
-                        "isni": "000000012156142X"
-                    }
-                }]
-            }],
-            "title": "InvenioRDM",
-            "additional_titles": [{
-                "title": "a research data management platform",
-                "type": "subtitle",
-                "lang": "eng"
-            }],
-            "publisher": "InvenioRDM",
-            "publication_date": "2018/2020-09",
-            "subjects": [{
-                "subject": "test",
-                "identifier": "test",
-                "scheme": "dewey"
-            }],
-            "contributors": [{
-                "name": "Nielsen, Lars Holm",
-                "type": "personal",
-                "role": "other",
-                "given_name": "Lars Holm",
-                "family_name": "Nielsen",
-                "identifiers": {
-                    "orcid": "0000-0001-8135-3489"
-                },
-                "affiliations": [{
-                    "name": "CERN",
-                    "identifiers": {
-                        "ror": "01ggx4157",
-                        "isni": "000000012156142X"
-                    }
-                }]
-            }],
-            "dates": [{
-                "date": "1939/1945",
-                "type": "other",
-                "description": "A date"
-            }],
-            "languages": [{
-                "id": "da",
-                "title": {"en": "Danish"}
-            }, {
-                "id": "en",
-                "title": {"en": "English"}
-            }],
-            "identifiers": [{
-                "identifier": "1924MNRAS..84..308E",
-                "scheme": "bibcode"
-            }],
-            "related_identifiers": [{
-                "identifier": "10.1234/foo.bar",
-                "scheme": "doi",
-                "relation": "cites",
-                "resource_type": {"type": "dataset"}
-            }],
-            "sizes": [
-                "11 pages"
-            ],
-            "formats": [
-                "application/pdf"
-            ],
-            "version": "v1.0",
-            "rights": [{
-                "rights": "Creative Commons Attribution 4.0 International",
-                "scheme": "spdx",
-                "identifier": "cc-by-4.0",
-                "url": "https://creativecommons.org/licenses/by/4.0/"
-            }],
-            "description": "Test",
-            "additional_descriptions": [{
-                "description": "Bla bla bla",
-                "type": "methods",
-                "lang": "eng"
-            }],
-            "locations": [{
-                "point": {
-                    "lat": 1,
-                    "lon": 2
-                },
-                "place": "home",
-                "description": "test"
-            }],
-            "funding": [{
-                "funder": {
-                    "name": "European Commission",
-                    "identifier": "1234",
-                    "scheme": "ror"
-                },
-                "award": {
-                    "title": "OpenAIRE",
-                    "number": "246686",
-                    "identifier": ".../246686",
-                    "scheme": "openaire"
-                }
-            }],
-            "references": [{
-                "reference": "Nielsen et al,..",
-                "identifier": "101.234",
-                "scheme": "doi"
-            }]
-        },
-        "ext": {
-            "dwc": {
-                "collectionCode": "abc",
-                "collectionCode2": 1.1,
-                "collectionCode3": True,
-                "test": ["abc", 1, True]
-            }
-        },
-        "provenance": {
-            "created_by": {
-                "user": 1
-            },
-            "on_behalf_of": {
-                "user": 2
-            }
-        },
-        "access": {
-            "record": "public",
-            "files": "restricted",
-            "embargo": {
-                "active": True,
-                "until": "2131-01-01",
-                "reason": "Only for medical doctors."
-            },
-        },
-        "files": {
-            "disabled": False,
-            "total_size": 1114324524355,
-            "count": 1,
-            "bucket": "81983514-22e5-473a-b521-24254bd5e049",
-            "files": [{
-                "checksum": "md5:234245234213421342",
-                "size": 1114324524355,
-                "key": "big-dataset.zip",
-                "ext": "zip",
-                "description": "File containing the data.",
-                "order": "1",
-                "default_preview": True,
-                "identifier": "445aaacd-9de1-41ab-af52-25ab6cb93df7"
-            }]
-        },
-        "notes": [
-            "Under investigation for copyright infringement."
-        ]
-    }
-
-
 @pytest.fixture()
 def headers():
     """Default headers for making requests."""
@@ -250,3 +61,53 @@ def headers():
         'content-type': 'application/json',
         'accept': 'application/json',
     }
+
+
+@pytest.fixture(scope='function')
+def full_record_ui(full_record):
+    """Full record dereferenced data, as is expeceted by the UI serializer."""
+    ui_record = deepcopy(full_record)
+    ui_record["metadata"]["creators"] = [{
+        "name": "Nielsen, Lars Holm",
+        "type": "personal",
+        "given_name": "Lars Holm",
+        "family_name": "Nielsen",
+        "identifiers": {
+            "orcid": "0000-0001-8135-3489"
+        },
+        "affiliations": [{
+            "name": "CERN",
+            "identifiers": {
+                "ror": "01ggx4157",
+                "isni": "000000012156142X"
+            }
+        }]
+    }]
+
+    ui_record["metadata"]["contributors"] = [{
+        "name": "Nielsen, Lars Holm",
+        "type": "personal",
+        "role": "other",
+        "given_name": "Lars Holm",
+        "family_name": "Nielsen",
+        "identifiers": {
+            "orcid": "0000-0001-8135-3489"
+        },
+        "affiliations": [{
+            "name": "CERN",
+            "identifiers": {
+                "ror": "01ggx4157",
+                "isni": "000000012156142X"
+            }
+        }]
+    }]
+
+    ui_record["metadata"]["languages"] = [{
+        "id": "da",
+        "title": {"en": "Danish"}
+    }, {
+        "id": "en",
+        "title": {"en": "English"}
+    }]
+
+    return ui_record
---
+++ tests/resources/serializers/test_datacite_serializer.py
@@ -0,0 +1,151 @@
+
+# -*- coding: utf-8 -*-
+#
+# Copyright (C) 2021 CERN.
+# Copyright (C) 2021 Caltech.
+#
+# Invenio-RDM-Records is free software; you can redistribute it and/or modify
+# it under the terms of the MIT License; see LICENSE file for more details.
+
+"""Resources serializers tests."""
+
+import json
+
+from invenio_rdm_records.resources.serializers import DataCite43JSONSerializer
+
+
+def test_datacite43_serializer(app, full_record):
+    """Test serializer to DayaCide 4.3 JSON"""
+    expected_data = {
+        "types": {
+            "resourceTypeGeneral": "FIXME",
+            "resourceType": "FIXME"
+        },
+        "creators": [
+            {
+                "name": "Nielsen, Lars Holm",
+                "nameType": "Personal",
+                "givenName": "Lars Holm",
+                "familyName": "Nielsen",
+                "nameIdentifiers": [{
+                    "nameIdentifier": "http://orcid.org/0000-0001-8135-3489",
+                    "nameIdentifierScheme": "ORCID",
+                    'schemeURI': 'http://orcid.org/'
+                }],
+                "affiliations": [
+                    {
+                        "name": "CERN",
+                        "affiliationIdentifier": "https://ror.org/01ggx4157",
+                        "affiliationIdentifierScheme": "ROR",
+                    }
+                ],
+            }
+        ],
+        "titles": [
+            {"title": "InvenioRDM"},
+            {
+                "title": "a research data management platform",
+                "titleType": "Subtitle",
+                "lang": "eng",
+            },
+        ],
+        "publisher": "InvenioRDM",
+        # "publicationYear": "2018",
+        "subjects": [{
+            "subject": "test",
+            "valueURI": "test",
+            "subjectScheme": "dewey"
+        }],
+        "contributors": [
+            {
+                "name": "Nielsen, Lars Holm",
+                "nameType": "Personal",
+                "contributorType": "Other",
+                "givenName": "Lars Holm",
+                "familyName": "Nielsen",
+                "nameIdentifiers": [{
+                    "nameIdentifier": "http://orcid.org/0000-0001-8135-3489",
+                    "nameIdentifierScheme": "ORCID",
+                    'schemeURI': 'http://orcid.org/'
+                }],
+                "affiliations": [
+                    {
+                        "name": "CERN",
+                        "affiliationIdentifier": "https://ror.org/01ggx4157",
+                        "affiliationIdentifierScheme": "ROR",
+                    }
+                ],
+            }
+        ],
+        "dates": [
+            {"date": "2018/2020-09", "dateType": "Issued"},
+            {
+                "date": "1939/1945",
+                "dateType": "Other",
+                "dateInformation": "A date"
+            },
+        ],
+        "language": "da",
+        "identifiers": [
+            {
+                "identifier": "1924MNRAS..84..308E",
+                "identifierType": "bibcode"
+            },
+            {
+                "identifier": "10.5281/inveniordm.1234",
+                "identifierType": "DOI"
+            },
+        ],
+        "relatedIdentifiers": [
+            {
+                "relatedIdentifier": "10.1234/foo.bar",
+                "relatedIdentifierType": "DOI",
+                "relationType": "Cites",
+                "resourceTypeGeneral": "dataset",
+            }
+        ],
+        "sizes": ["11 pages"],
+        "formats": ["application/pdf"],
+        "version": "v1.0",
+        "rightsList": [
+            {
+                "rights": "Creative Commons Attribution 4.0 International",
+                "rightsIdentifierScheme": "spdx",
+                "rightsIdentifier": "cc-by-4.0",
+                "rightsUri": "https://creativecommons.org/licenses/by/4.0/",
+            }
+        ],
+        "descriptions": [
+            {"description": "Test", "descriptionType": "Abstract"},
+            {
+                "description": "Bla bla bla",
+                "descriptionType": "Methods",
+                "lang": "eng"
+            },
+        ],
+        "geoLocations": [
+            {
+                "geoLocationPoint": {
+                    'pointLatitude': -32.94682,
+                    'pointLongitude': -60.63932
+                },
+                "geoLocationPlace": "test location place",
+            }
+        ],
+        "fundingReferences": [
+            {
+                "funderName": "European Commission",
+                "funderIdentifier": "1234",
+                "funderIdentifierType": "ROR",
+                "awardTitle": "OpenAIRE",
+                "awardNumber": "246686",
+                "awardURI": ".../246686",
+            }
+        ],
+        "schemaVersion": "http://datacite.org/schema/kernel-4",
+    }
+
+    serializer = DataCite43JSONSerializer()
+    serialized_record = serializer.dump_one(full_record)
+
+    assert serialized_record["metadata"] == expected_data
