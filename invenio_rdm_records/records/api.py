1e7a9f3fa8d63b5354db80045e40f0b7f16d54cf
Guillaume Viger <fenekku@fenekku.com>
2021-06-03 16:51:22 -0400

    subjects: update ES mappings

--- invenio_rdm_records/records/api.py
+++ invenio_rdm_records/records/api.py
@@ -112,7 +112,7 @@ class RDMDraft(CommonFieldsMixin, Draft):
     model_cls = models.RDMDraftMetadata
 
     index = IndexField(
-        "rdmrecords-drafts-draft-v3.0.0", search_alias="rdmrecords"
+        "rdmrecords-drafts-draft-v4.0.0", search_alias="rdmrecords"
     )
 
     files = FilesField(
@@ -141,7 +141,7 @@ class RDMRecord(CommonFieldsMixin, Record):
     model_cls = models.RDMRecordMetadata
 
     index = IndexField(
-        "rdmrecords-records-record-v3.0.0", search_alias="rdmrecords-records"
+        "rdmrecords-records-record-v4.0.0", search_alias="rdmrecords-records"
     )
 
     files = FilesField(
