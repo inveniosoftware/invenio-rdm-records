23541db98fbb6ebd84f3bf7ed1aa38725fc4aea1
Lars Holm Nielsen <lars.holm.nielsen@cern.ch>
2021-07-14 12:22:41 +0200

    tests: fix ./run-test.sh to work on all versions

--- run-tests.sh
+++ run-tests.sh
@@ -54,6 +54,8 @@ fi
 python -m check_manifest --ignore ".*-requirements.txt"
 python -m sphinx.cmd.build -qnNW docs docs/_build/html
 eval "$(docker-services-cli up --db ${DB:-postgresql} --search ${SEARCH:-elasticsearch} --mq ${MQ:-redis} --env)"
-python -m pytest "${pytest_args[@]}"
+# Note: expansion of pytest_args looks like below to not cause an unbound
+# variable error when 1) "nounset" and 2) the array is empty.
+python -m pytest ${pytest_args[@]+"${pytest_args[@]}"}
 tests_exit_code=$?
 exit "$tests_exit_code"
