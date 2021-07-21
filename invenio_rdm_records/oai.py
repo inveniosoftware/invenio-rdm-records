3066b82ccc4ec3d68aa7ed86b3789a6041c046d4
David Eckhard <dabombeeer@gmail.com>
2021-07-21 08:52:48 +0200

    oai: add rdm specific getrecord_fetcher function fix: return public records only

--- invenio_rdm_records/oai.py
+++ invenio_rdm_records/oai.py
@@ -9,11 +9,22 @@
 """Invenio-RDM-Records OAI Functionality."""
 
 from elasticsearch_dsl import Q
-from invenio_pidstore.errors import PersistentIdentifierError
+from flask import g
+from invenio_pidstore.errors import PersistentIdentifierError, \
+    PIDDoesNotExistError
 from invenio_pidstore.fetchers import FetchedPID
+from invenio_pidstore.models import PersistentIdentifier
+from invenio_records_resources.services.errors import PermissionDeniedError
 from invenio_search import RecordsSearch
 
+from .proxies import current_rdm_records
+from .resources.serializers.dublincore import DublinCoreXMLSerializer
 from .services.pids.providers.oai import OAIPIDProvider
+
+
+def dublincore_etree(pid, record):
+    """Get DublinCore XML etree for OAI-PMH."""
+    return DublinCoreXMLSerializer().serialize_object_xml(record["_source"])
 
 
 def oaiid_fetcher(record_uuid, data):
@@ -31,7 +42,24 @@ def oaiid_fetcher(record_uuid, data):
         provider=OAIPIDProvider,
         pid_type="oai",
         pid_value=str(pid_value),
-    )    
+    )
+
+
+def getrecord_fetcher(record_id):
+    """Fetch record data as dict with identity check for serialization."""
+    recid = PersistentIdentifier.get_by_object(
+        pid_type='recid', object_uuid=record_id, object_type='rec'
+    )
+
+    try:
+        result = current_rdm_records.records_service.read(
+            recid.pid_value, g.identity
+        )
+    except PermissionDeniedError:
+        # if it is a restricted record.
+        raise PIDDoesNotExistError('recid', None)
+
+    return result.to_dict()
 
 
 class OAIRecordSearch(RecordsSearch):
@@ -40,4 +68,7 @@ class OAIRecordSearch(RecordsSearch):
     class Meta:
         """Configuration for OAI server search."""
 
-        default_filter = Q('exists', field='pids.oai.identifier')
+        default_filter = [
+            Q('exists', field='pids.oai.identifier'),
+            Q('term', **{'access.record': 'public'}),
+        ]
