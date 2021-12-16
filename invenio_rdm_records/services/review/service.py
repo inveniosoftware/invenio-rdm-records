a64b9c655b909e8b0f81a912e8b34b369a0ac462
Guillaume Viger <fenekku@fenekku.com>
2021-12-16 18:50:35 -0500

    release: v0.34.2

--- invenio_rdm_records/services/review/service.py
+++ invenio_rdm_records/services/review/service.py
@@ -1,6 +1,7 @@
 # -*- coding: utf-8 -*-
 #
 # Copyright (C) 2021 CERN.
+# Copyright (C) 2021 Northwestern University.
 #
 # Invenio-RDM-Records is free software; you can redistribute it and/or modify
 # it under the terms of the MIT License; see LICENSE file for more details.
