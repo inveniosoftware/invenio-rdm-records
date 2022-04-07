5338bb4130cd87140f50a6be29b881483dc984d0
Pablo Panero <pablo.panero@cern.ch>
2022-04-07 15:37:15 +0200

    tasks: add vocabulary updates propagation

--- invenio_rdm_records/services/tasks.py
+++ invenio_rdm_records/services/tasks.py
@@ -7,12 +7,14 @@
 
 """Celery tasks."""
 
+import arrow
 from celery import shared_task
 from flask import current_app
 from invenio_access.permissions import system_identity
 
 from invenio_rdm_records.proxies import current_rdm_records
 from invenio_rdm_records.services.errors import EmbargoNotLiftedError
+from invenio_records_resources.proxies import current_service_registry
 
 
 @shared_task(ignore_result=True)
@@ -28,3 +30,20 @@ def update_expired_embargos():
             current_app.logger.warning(
                 f"Embargo from record with id {record['id']} was not lifted")
             continue
+
+
+@shared_task(ignore_result=True)
+def propagate_updated_vocabularies(type, records):
+    """Propagates the update of vocabulary records to bibliographic reocrds.
+
+    :param type: vocabulary type, necessary to pick the service.
+    :param records: vocabulary records that were updated.
+    """
+    task_start = arrow.utcnow().strftime("%Y-%m-%dT%H:%M:%S.%f")
+    service = current_service_registry.get(type)
+    build_query() # we need the UUID and the version_id of each record
+
+    # Q: We could do a scan() and use the indexer from here.
+    # avoiding an extra step for the service.reindex + queue/consumption
+    # Does this bypass preparing the record? I think not.
+
