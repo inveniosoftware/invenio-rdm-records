5ab1eb28be737d713ef60492e1002edc99230b61
Pablo Panero <pablo.panero@cern.ch>
2022-08-04 16:16:49 +0200

    services: create generic component for custom fields and metadata

--- invenio_rdm_records/services/components/metadata.py
+++ invenio_rdm_records/services/components/metadata.py
@@ -1,6 +1,6 @@
 # -*- coding: utf-8 -*-
 #
-# Copyright (C) 2020-2021 CERN.
+# Copyright (C) 2020-2022 CERN.
 # Copyright (C) 2020 Northwestern University.
 # Copyright (C) 2021 TU Wien.
 #
@@ -17,28 +17,30 @@ from invenio_drafts_resources.services.records.components import ServiceComponen
 class MetadataComponent(ServiceComponent):
     """Service component for metadata."""
 
+    field = "metadata"
     new_version_skip_fields = ["publication_date", "version"]
 
     def create(self, identity, data=None, record=None, **kwargs):
         """Inject parsed metadata to the record."""
-        record.metadata = data.get("metadata", {})
+        setattr(record, self.field, data.get(self.field, {}))
 
     def update_draft(self, identity, data=None, record=None, **kwargs):
         """Inject parsed metadata to the record."""
-        record.metadata = data.get("metadata", {})
+        setattr(record, self.field, data.get(self.field, {}))
 
     def publish(self, identity, draft=None, record=None, **kwargs):
         """Update draft metadata."""
-        record.metadata = draft.get("metadata", {})
+        setattr(record, self.field, draft.get(self.field, {}))
 
     def edit(self, identity, draft=None, record=None, **kwargs):
         """Update draft metadata."""
-        draft.metadata = record.get("metadata", {})
+        setattr(draft, self.field, record.get(self.field, {}))
 
     def new_version(self, identity, draft=None, record=None, **kwargs):
         """Update draft metadata."""
-        draft.metadata = copy(record.get("metadata", {}))
+        setattr(draft, self.field, copy(record.get(self.field, {})))
         # Remove fields that should not be copied to the new version
         # (publication date and version)
+        field_values = getattr(draft, self.field)
         for f in self.new_version_skip_fields:
-            draft.metadata.pop(f, None)
+            field_values.pop(f, None)
