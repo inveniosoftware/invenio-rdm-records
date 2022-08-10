673bf941fb927bf42fe7652b6d998bc04c75465c
nico <nicolas.rodriguezcuellar@student.hu.nl>
2022-08-10 15:02:54 +0200

    oai-pmh: add administration list view
    * add oaipmh demo data

---
+++ invenio_rdm_records/administration/views/__init__.py
@@ -0,0 +1,9 @@
+# -*- coding: utf-8 -*-
+#
+# Copyright (C) 2022 CERN.
+#
+# invenio-administration is free software; you can redistribute it and/or
+# modify it under the terms of the MIT License; see LICENSE file for more
+# details.
+
+"""Invenio administration views module for RDM records."""
