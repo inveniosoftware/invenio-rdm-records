5a1a6ab1bc6da0d5cc5e817eb605a12bc3ddcbd0
kpsherva <karolina.m.przerwa@gmail.com>
2022-09-06 17:17:04 +0200

    administration: add edit and create views
    * refactor schema

--- invenio_rdm_records/administration/views/oai.py
+++ invenio_rdm_records/administration/views/oai.py
@@ -8,7 +8,7 @@
 
 """Invenio administration OAI-PMH view module."""
 from invenio_administration.views.base import AdminResourceListView,\
-    AdminResourceEditView
+    AdminResourceEditView, AdminResourceCreateView
 
 
 class OaiPmhListView(AdminResourceListView):
@@ -18,7 +18,7 @@ class OaiPmhListView(AdminResourceListView):
     name = "OAI-PMH"
     resource_config = "oaipmh_server_resource"
     title = "OAI-PMH Sets"
-    category = "Exports"
+    category = "Site management"
     pid_path = "id"
 
     # OAI sets are not searchable in ES
@@ -51,3 +51,30 @@ class OaiPmhListView(AdminResourceListView):
     search_config_name = "RDM_OAI_PMH_SEARCH"
     search_facets_config_name = "RDM_OAI_PMH_FACETS"
     search_sort_config_name = "RDM_OAI_PMH_SORT_OPTIONS"
+
+    create_view_name = "oaipmh_create"
+    list_view_name = "OAI-PMH"
+
+
+class OaiPmhEditView(AdminResourceEditView):
+
+    name = "oaipmh_edit"
+    url = "/oai-pmh/<pid_value>/edit"
+    resource_config = "oaipmh_server_resource"
+    pid_path = "id"
+    api_endpoint = "/oaipmh/sets"
+    title = "Edit OAI-PMH set"
+
+    list_view_name = "OAI-PMH"
+
+
+class OaiPmhCreateView(AdminResourceCreateView):
+
+    name = "oaipmh_create"
+    url = "/oai-pmh/create"
+    resource_config = "oaipmh_server_resource"
+    pid_path = "id"
+    api_endpoint = "/oaipmh/sets"
+    title = "Create OAI-PMH set"
+
+    list_view_name = "OAI-PMH"
