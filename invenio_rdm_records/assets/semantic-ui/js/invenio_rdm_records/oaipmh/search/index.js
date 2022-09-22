a7632861f8dcb9a183d79e6aa83ecc2a1f95e198
pablo <pineirin@gmail.com>
2022-09-22 12:12:55 +0200

    Administration: fix broken profile menu in oai-pmh pages
    * closes: inveniosoftware/invenio-administration#97

--- invenio_rdm_records/assets/semantic-ui/js/invenio_rdm_records/oaipmh/search/index.js
+++ invenio_rdm_records/assets/semantic-ui/js/invenio_rdm_records/oaipmh/search/index.js
@@ -4,7 +4,7 @@
 // Invenio RDM is free software; you can redistribute it and/or modify it
 // under the terms of the MIT License; see LICENSE file for more details.
 
-import defaultComponents from "@js/invenio_administration/search/search.js";
+import defaultComponents from "@js/invenio_administration/src/search/search.js";
 import { createSearchAppInit } from "@js/invenio_search_ui";
 import { SearchResultItem } from "./SearchResultItem";
 import { parametrize } from "react-overridable";
