29e082667c66cdf5e1f7ce2b9e9298eec7369a73
Manuel Alejandro <mumox7@gmail.com>
2022-09-22 15:52:29 +0100

    links: added prefix dropdown to links table.

--- invenio_rdm_records/assets/semantic-ui/js/invenio_rdm_records/oaipmh/details/LinksTable.js
+++ invenio_rdm_records/assets/semantic-ui/js/invenio_rdm_records/oaipmh/details/LinksTable.js
@@ -5,32 +5,111 @@
 // under the terms of the MIT License; see LICENSE file for more details.
 
 import PropTypes from "prop-types";
-import React from "react";
+import React, { useState } from "react";
 import { i18next } from "@translations/invenio_administration/i18next";
 import CopyButton from "./CopyButton";
-import { Table, Header } from "semantic-ui-react";
+import { Table, Header, Dropdown, Grid } from "semantic-ui-react";
+import { prefixes } from "./formats";
+
+const prefixFormats = [
+  {
+    key: prefixes.DUBLIN_CORE,
+    text: "OAI Dublin Core",
+    value: prefixes.DUBLIN_CORE
+  },
+  {
+    key: prefixes.OAI_DATACITE,
+    text: "OAI DataCite",
+    value: prefixes.OAI_DATACITE
+  },
+  {
+    key: prefixes.DATACITE,
+    text: "DataCite",
+    value: prefixes.DATACITE
+  }
+]
 
 function LinksTable({ data }) {
+  const [links, setLinks] = useState(data.links);
+
   /**
-   * WIP: this method should remove metadataPrefix from the url once the attribute is listed
-   * as a dropdown.
-   *
-   * @param {string} url
-   * @returns {string} stripped url
+   * Replaces the metadata prefix in the link.
+   * Used to update links when the metadata prefix is changed.
    */
-  const sanitizeLink = (url) => {
-    return url;
+  const replaceLinkPrefix = (link, newPrefix) => {
+    const oldPrefix = getPrefixFromLink(link);
+    if (oldPrefix) {
+      return link.replace(oldPrefix, newPrefix)
+    }
+    return link;
   };
 
-  const { links } = data;
-  const listRecords = sanitizeLink(links["oai-listrecords"]);
-  const listIdentifiers = sanitizeLink(links["oai-listidentifiers"]);
+  /**
+   * Retrieves metadata prefix from a link.
+   * Uses a regex to retrieve the prefix, matching "&metadataPrefix=...&".
+   * Returns null if not found.
+   */
+  const getPrefixFromLink = (link) => {
+    const prefixRegex = /\&metadataPrefix\=(.*)\&/;
+    const match = link.match(prefixRegex);
+    if (match && match.length) {
+      const prefix = match[1];
+      return prefix;
+    }
+    return null;
+  };
+
+  /**
+   * Dropdown's on change handler.
+   * This method will update the following states if the new prefix is known:
+   * - 'links', each link with its prefix replaced by the new prefix.
+   */
+  const prefixOnChange = (event, data) => {
+    const newPrefix = data.value;
+    if (Object.values(prefixes).includes(newPrefix)) {
+      const newLinks = {};
+      Object.keys(links).map((key) => {
+        let link = links[key];
+        let newLink = replaceLinkPrefix(link, newPrefix);
+        newLinks[key] = newLink;
+      });
+      setLinks(newLinks);
+    }
+  };
+
+  const listRecords = links["oai-listrecords"];
+  const listIdentifiers = links["oai-listidentifiers"];
+
+  // Set default prefix based on backend link, use dublin core if unknown.
+  let defaultPrefix = getPrefixFromLink(listRecords)
+  if (!Object.prototype.hasOwnProperty.call(prefixes, defaultPrefix)) {
+    defaultPrefix = prefixes.DUBLIN_CORE;
+  }
 
   return (
     <>
       {
         <>
-          <Header as="h2">{i18next.t("Links")}</Header>
+          <Grid>
+            <Grid.Row />
+            <Grid.Row>
+              <Grid.Column width={3}>
+                <Header as="h2">{i18next.t("Links")}</Header>
+              </Grid.Column>
+              <Grid.Column width={13}>
+                <span className="mr-10" basic>
+                  { i18next.t("Format") }
+                </span>
+                <Dropdown
+                  options={prefixFormats}
+                  floating
+                  button
+                  defaultValue={defaultPrefix}
+                  onChange={prefixOnChange}
+                />
+              </Grid.Column>
+            </Grid.Row>
+          </Grid>
           <Table unstackable>
             <Table.Body>
               <Table.Row key="list-records">
