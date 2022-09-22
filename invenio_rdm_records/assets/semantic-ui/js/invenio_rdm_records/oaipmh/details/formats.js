5e620e06fcf3a6ec01a79f7d5aa5e9ec74114649
Manuel Alejandro <mumox7@gmail.com>
2022-09-22 15:52:29 +0100

    links: added prefix dropdown to links table.

---
+++ invenio_rdm_records/assets/semantic-ui/js/invenio_rdm_records/oaipmh/details/formats.js
@@ -0,0 +1,15 @@
+const DUBLIN_CORE  = "oai_dc";
+/** */
+
+const DATACITE = "datacite";
+/** */
+
+const OAI_DATACITE = "oai_datacite";
+/** */
+
+export const prefixes = {
+  DUBLIN_CORE,
+  DATACITE,
+  OAI_DATACITE
+};
+/** */
