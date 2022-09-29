269666fef8a7fed5d6999e6b4b4b106e36efd8b6
Maximilian Moser <maximilian.moser@tuwien.ac.at>
2022-09-29 19:47:43 +0200

    global: add support for opensearch 2.x

---
+++ invenio_rdm_records/records/mappings/os-v2/__init__.py
@@ -0,0 +1,8 @@
+# -*- coding: utf-8 -*-
+#
+# Copyright (C) 2022 CERN.
+#
+# Invenio-RDM-Records is free software; you can redistribute it and/or modify
+# it under the terms of the MIT License; see LICENSE file for more details.
+
+"""RDM Records OpenSearch v2 mappings."""
