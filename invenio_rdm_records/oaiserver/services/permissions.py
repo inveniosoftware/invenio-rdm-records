8ead36d2b44f725a3043d8a4d8d166b6d6129905
jrcastro2 <jrcastro9515@gmail.com>
2022-09-30 14:23:43 +0200

    permissions: add administration to OAI
    * closes https://github.com/inveniosoftware/invenio-administration/issues/128

--- invenio_rdm_records/oaiserver/services/permissions.py
+++ invenio_rdm_records/oaiserver/services/permissions.py
@@ -6,7 +6,7 @@
 # it under the terms of the MIT License; see LICENSE file for more details.
 
 """Permissions for OAI-PMH service."""
-
+from invenio_administration.generators import Administration
 from invenio_records_permissions import BasePermissionPolicy
 from invenio_records_permissions.generators import Admin, AnyUser, SystemProcess
 
@@ -14,8 +14,8 @@ from invenio_records_permissions.generators import Admin, AnyUser, SystemProcess
 class OAIPMHServerPermissionPolicy(BasePermissionPolicy):
     """OAI-PMH permission policy."""
 
-    can_read = [Admin(), SystemProcess()]
-    can_create = [Admin(), SystemProcess()]
-    can_delete = [Admin(), SystemProcess()]
-    can_update = [Admin(), SystemProcess()]
-    can_read_format = [Admin(), SystemProcess()]
+    can_read = [Admin(), Administration(), SystemProcess()]
+    can_create = [Admin(), Administration(), SystemProcess()]
+    can_delete = [Admin(), Administration(), SystemProcess()]
+    can_update = [Admin(), Administration(), SystemProcess()]
+    can_read_format = [Admin(), Administration(), SystemProcess()]
