1f29bc96a0e5330a00c11cbd21024c391f16ae5f
Manuel Alejandro <alejandromumo>
2022-10-17 18:01:00 +0200

    Links: table refactor as class component
    * reformat details links fetch
    * add translations to javascript
    * closes: #1087
    
    Co-authored-by: Pablo García Marcos <pineirin@gmail.com>

---
+++ invenio_rdm_records/assets/semantic-ui/translations/invenio_rdm_records/i18next-scanner.config.js
@@ -0,0 +1,63 @@
+// This file is part of InvenioRdmRecords
+// Copyright (C) 2022 CERN.
+//
+// Invenio RDM is free software; you can redistribute it and/or modify it
+// under the terms of the MIT License; see LICENSE file for more details.
+
+// list of func used to
+// mark the strings for translation
+const { languages } = require("./package.json").config;
+const funcList = ["i18next.t"];
+const extensions = ['.js', '.jsx'];
+
+module.exports = {
+  options: {
+    debug: true,
+    removeUnusedKeys: true,
+    browserLanguageDetection: true,
+    func: {
+      list: funcList,
+      extensions: extensions,
+    },
+      //using Trans component
+    trans: {
+      component: 'Trans',
+      extensions: extensions,
+      fallbackKey: function (ns, value) {
+        return value;
+      },
+    },
+    lngs: languages,
+    ns: [
+      // file name (.json)
+      "translations",
+    ],
+    defaultLng: "en",
+    defaultNs: "translations",
+    // @param {string} lng The language currently used.
+    // @param {string} ns The namespace currently used.
+    // @param {string} key The translation key.
+    // @return {string} Returns a default value for the translation key.
+    defaultValue: function (lng, ns, key) {
+      if (lng === "en") {
+        // Return key as the default value for English language
+        return key;
+      }
+      return "";
+    },
+    resource: {
+      // The path where resources get loaded from. Relative to current working directory.
+      loadPath: "messages/{{lng}}/{{ns}}.json",
+
+      // The path to store resources.
+      savePath: "messages/{{lng}}/{{ns}}.json",
+      jsonIndent: 2,
+      lineEnding: "\n",
+    },
+    nsSeparator: false, // namespace separator
+
+    //Set to false to disable key separator
+    // if you prefer having keys as the fallback for translation (e.g. gettext).
+    keySeparator: false,
+  },
+};
---
+++ invenio_rdm_records/assets/semantic-ui/translations/invenio_rdm_records/i18next.js
@@ -0,0 +1,37 @@
+// This file is part of InvenioRdmRecords
+// Copyright (C) 2022 CERN.
+//
+// Invenio RDM is free software; you can redistribute it and/or modify it
+// under the terms of the MIT License; see LICENSE file for more details.
+
+import i18n from "i18next";
+
+import LanguageDetector from "i18next-browser-languagedetector";
+import { translations } from "./messages";
+import { initReactI18next } from "react-i18next";
+
+const options = {
+  fallbackLng: "en", // fallback keys
+  returnEmptyString: false,
+  debug: process.env.NODE_ENV === "development",
+  resources: translations,
+  keySeparator: false,
+  nsSeparator: false,
+  // specify language detection order
+  detection: {
+    order: ["htmlTag"],
+    // cache user language off
+    caches: [],
+  },
+  react: {
+    // Set empty - to allow html tags convert to trans tags
+    // HTML TAG | Trans TAG
+    //  <span>  | <1>
+    transKeepBasicHtmlNodesFor: [],
+  },
+};
+
+const i18next = i18n.createInstance();
+i18next.use(LanguageDetector).use(initReactI18next).init(options);
+
+export { i18next };
