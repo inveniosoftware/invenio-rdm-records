ce9dcb79f33140afd0aaeeb4b324bab9365f41b9
Maximilian Moser <maximilian.moser@tuwien.ac.at>
2022-11-02 12:54:54 +0100

    permissions: disable write operations when read-only mode is on

--- invenio_rdm_records/oaiserver/services/permissions.py
+++ invenio_rdm_records/oaiserver/services/permissions.py
@@ -1,6 +1,7 @@
 # -*- coding: utf-8 -*-
 #
 # Copyright (C) 2022 Graz University of Technology.
+# Copyright (C) 2022-2024 TU Wien.
 #
 # Invenio-RDM-Records is free software; you can redistribute it and/or modify
 # it under the terms of the MIT License; see LICENSE file for more details.
@@ -8,14 +9,14 @@
 """Permissions for OAI-PMH service."""
 from invenio_administration.generators import Administration
 from invenio_records_permissions import BasePermissionPolicy
-from invenio_records_permissions.generators import SystemProcess
+from invenio_records_permissions.generators import DisableIfReadOnly, SystemProcess
 
 
 class OAIPMHServerPermissionPolicy(BasePermissionPolicy):
     """OAI-PMH permission policy."""
 
     can_read = [Administration(), SystemProcess()]
-    can_create = [Administration(), SystemProcess()]
-    can_delete = [Administration(), SystemProcess()]
-    can_update = [Administration(), SystemProcess()]
+    can_create = [Administration(), SystemProcess(), DisableIfReadOnly()]
+    can_delete = [Administration(), SystemProcess(), DisableIfReadOnly()]
+    can_update = [Administration(), SystemProcess(), DisableIfReadOnly()]
     can_read_format = [Administration(), SystemProcess()]
