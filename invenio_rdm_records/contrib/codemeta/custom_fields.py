0891621edf2783eedc433264b8d549db3a68bca0
Karolina Przerwa <karolina.m.przerwa@gmail.com>
2023-03-03 10:45:58 +0100

    custom fields: fix deprecated import

--- invenio_rdm_records/contrib/codemeta/custom_fields.py
+++ invenio_rdm_records/contrib/codemeta/custom_fields.py
@@ -14,7 +14,7 @@ Implements the following fields:
 - Development status
 """
 
-from flask_babelex import _
+from invenio_i18n import lazy_gettext as _
 from invenio_records_resources.services.custom_fields import KeywordCF
 from invenio_records_resources.services.records.facets import CFTermsFacet
 from invenio_vocabularies.services.custom_fields import VocabularyCF
