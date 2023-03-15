cbec410f88c27c3473f7601b37ccde9c32042b28
Pablo Panero <pablo.panero@cern.ch>
2023-03-15 17:23:36 +0100

    contrib: add journal and meeting sort options

--- invenio_rdm_records/contrib/journal/__init__.py
+++ invenio_rdm_records/contrib/journal/__init__.py
@@ -6,10 +6,17 @@
 # it under the terms of the MIT License; see LICENSE file for more details.
 
 """Journal module."""
+
 from .custom_fields import (
     JOURNAL_CUSTOM_FIELDS,
     JOURNAL_CUSTOM_FIELDS_UI,
     JOURNAL_NAMESPACE,
 )
+from .sort import JOURNAL_SORT_OPTIONS
 
-__all_ = (JOURNAL_CUSTOM_FIELDS, JOURNAL_CUSTOM_FIELDS_UI, JOURNAL_NAMESPACE)
+__all__ = (
+    JOURNAL_CUSTOM_FIELDS,
+    JOURNAL_CUSTOM_FIELDS_UI,
+    JOURNAL_NAMESPACE,
+    JOURNAL_SORT_OPTIONS,
+)
