4b2cb46f95dbfc9dc9adba9e519f852176bdd4db
Maximilian Moser <maximilian.moser@tuwien.ac.at>
2023-03-21 12:26:49 +0100

    schemas: dump the 'stats' field of a record if it's present
    * this is required for displaying the statistics (views/downloads) on
      the search results, because the search ui uses the API behind the
      scenes, i.e. only what's being dumped for the API can be used
    * note that the stats will currently only be present in records being
      loaded from search results, because the search dumper extension adds
      the field but doesn't remove it on loading - the field isn't present
      on records loaded from the database!

--- invenio_rdm_records/services/schemas/stats.py
+++ invenio_rdm_records/services/schemas/stats.py
@@ -2,17 +2,32 @@
 #
 # Copyright (C) 2020 CERN.
 # Copyright (C) 2020 Northwestern University.
+# Copyright (C) 2023 TU Wien.
 #
 # Invenio-RDM-Records is free software; you can redistribute it and/or modify
 # it under the terms of the MIT License; see LICENSE file for more details.
 
-"""RDM record schemas."""
+"""Schema for record statistics."""
 
 from marshmallow import Schema, fields
 
 
-class StatsSchema(Schema):
-    """Files metadata schema."""
+class PartialStatsSchema(Schema):
+    """Schema for a part of the record statistics.
+
+    This fits both the statistics for "this version" as well as
+    "all versions", because they have the same shape.
+    """
 
     views = fields.Int()
+    unique_views = fields.Int()
     downloads = fields.Int()
+    unique_downloads = fields.Int()
+    data_volume = fields.Float()
+
+
+class StatsSchema(Schema):
+    """Schema for the entire record statistics."""
+
+    this_version = fields.Nested(PartialStatsSchema)
+    all_versions = fields.Nested(PartialStatsSchema)
