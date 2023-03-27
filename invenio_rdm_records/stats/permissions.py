fda382ad9c91e6c3ea2ec112563601df39fa4f14
Maximilian Moser <maximilian.moser@tuwien.ac.at>
2023-03-27 13:12:14 +0200

    stats: add translation layer for permissions
    * this new translation layer allows us to translate between the query
      parameters as given by the user to the stats API and something that's
      understood by the records service for permission checks
    * this enables us to use the permission policy for the statistics
      endpoint, and reuse existing permission generators

--- invenio_rdm_records/stats/permissions.py
+++ invenio_rdm_records/stats/permissions.py
@@ -11,14 +11,40 @@ In contrast to the very liberal defaults provided by Invenio-Stats, these permis
 factories deny access unless otherwise specified.
 """
 
+from flask import g
 from invenio_stats import current_stats
 
-DenyAllPermission = type(
-    "Deny",
-    (),
-    {"can": lambda self: False, "allows": lambda *args: False},
-)()
-"""The permission factory that denies without even blinking."""
+from ..proxies import current_rdm_records_service as records_service
+
+
+class StatsPermissionTranslator:
+    """Translator class between the statistics endpoint and the service layer."""
+
+    def __init__(self, action_name, service=None, **params):
+        """Constructor."""
+        self.service = service or records_service
+        self.action_name = action_name
+        self.params = params
+
+    def translate_params(self, identity):
+        """Translate the params between the stats endpoint and the records service.
+
+        This method acts as a translation layer between the statistics API endpoint
+        and the records service and is intended to be overridden by potential
+        subclasses. It takes the ``params`` from the query and translates
+        them into something that can be used by the records service (as keyword
+        arguments) to check for permissions.
+        For example, the query ``params`` might provide a ``recid`` value, but the
+        permission generators may require a ``record`` class. This could be retrieved
+        in this method.
+        """
+        return self.params
+
+    def can(self):
+        """Check if the action is allowed for the current identity."""
+        return self.service.check_permission(
+            g.identity, self.action_name, **self.translate_params(g.identity)
+        )
 
 
 def default_deny_permission_factory(query_name, params):
@@ -28,6 +54,6 @@ def default_deny_permission_factory(query_name, params):
     configured permission factory.
     """
     if current_stats.queries[query_name].permission_factory is None:
-        return DenyAllPermission
+        return StatsPermissionTranslator("query_stats", **params)
     else:
         return current_stats.queries[query_name].permission_factory(query_name, params)
