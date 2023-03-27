c3735046e91d3b87067a240591d5d640f9cfde3e
Maximilian Moser <maximilian.moser@tuwien.ac.at>
2023-03-27 11:24:38 +0200

    records: update source for stats system field
    * in order to be more consistent between search results and record
      details, we let the stats system field try to get the stats
      information from the records search index first
    * the (more up-to-date) aggregations index will only be used as a
      fallback

--- invenio_rdm_records/records/systemfields/statistics.py
+++ invenio_rdm_records/records/systemfields/statistics.py
@@ -8,12 +8,36 @@
 """Cached transient field for record statistics."""
 
 from invenio_records.systemfields import SystemField
+from invenio_search.proxies import current_search_client
+from invenio_search.utils import build_alias_name
 
 from ...stats.utils import get_record_stats
 
 
 class RecordStatisticsField(SystemField):
     """Field for lazy fetching and caching (but not storing) of record statistics."""
+
+    def _get_record_stats(self, record):
+        """Get the record's statistics from either record or aggregation index."""
+        stats = None
+        recid, parent_recid = record["id"], record.parent["id"]
+
+        try:
+            # for more consistency between search results and each record's details,
+            # we try to get the statistics from the record's search index first
+            # note: this field is dumped into the record's data before indexing
+            #       by the search dumper extension "StatisticsDumperExt"
+            res = current_search_client.get(
+                index=build_alias_name(record.index._name),
+                id=record.id,
+                params={"_source_includes": "stats"},
+            )
+            stats = res["_source"]["stats"]
+        except Exception:
+            stats = None
+
+        # as a fallback, use the more up-to-date aggregations indices
+        return stats or get_record_stats(recid=recid, parent_recid=parent_recid)
 
     #
     # Data descriptor methods (i.e. attribute access)
@@ -28,9 +52,7 @@ class RecordStatisticsField(SystemField):
         if stats:
             return stats
 
-        recid = record["id"]
-        parent_recid = record.parent["id"]
-        stats = get_record_stats(recid=recid, parent_recid=parent_recid)
+        stats = self._get_record_stats(record)
         record["stats"] = stats
 
         return stats
