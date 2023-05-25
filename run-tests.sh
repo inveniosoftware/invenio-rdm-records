0d6280e35f5f565be713a69d56948d4060ecfaeb
Alex Ioannidis <a.ioannidis@cern.ch>
2023-05-25 08:42:06 +0200

    tests: add Redis cache service

--- run-tests.sh
+++ run-tests.sh
@@ -54,7 +54,7 @@ fi
 export LC_TIME=en_US.UTF-8
 python -m check_manifest
 python -m sphinx.cmd.build -qnNW docs docs/_build/html
-eval "$(docker-services-cli up --db ${DB:-postgresql} --search ${SEARCH:-opensearch} --mq ${MQ:-redis} --env)"
+eval "$(docker-services-cli up --db ${DB:-postgresql} --search ${SEARCH:-opensearch} --mq ${MQ:-rabbitmq} --cache ${CACHE:-redis} --env)"
 # Note: expansion of pytest_args looks like below to not cause an unbound
 # variable error when 1) "nounset" and 2) the array is empty.
 python -m pytest ${pytest_args[@]+"${pytest_args[@]}"}
