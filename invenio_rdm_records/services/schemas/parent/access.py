61184e87e99f3e8bc97f3fb4a49070a0779104d3
anikachurilova <nyuta.churilova@gmail.com>
2023-07-24 13:20:53 +0200

    schemas: sanitize html for conditions text

--- invenio_rdm_records/services/schemas/parent/access.py
+++ invenio_rdm_records/services/schemas/parent/access.py
@@ -15,7 +15,12 @@
 from datetime import timezone
 
 from marshmallow import Schema, fields, validate
-from marshmallow_utils.fields import ISODateString, SanitizedUnicode, TZDateTime
+from marshmallow_utils.fields import (
+    ISODateString,
+    SanitizedHTML,
+    SanitizedUnicode,
+    TZDateTime,
+)
 from marshmallow_utils.permissions import FieldPermissionsMixin
 
 
@@ -65,8 +70,7 @@ class AccessSettingsSchema(Schema):
     allow_user_requests = fields.Boolean()
     allow_guest_requests = fields.Boolean()
 
-    # accept conditions text
-    accept_conditions_text = fields.String()
+    accept_conditions_text = SanitizedHTML()
 
 
 class ParentAccessSchema(Schema, FieldPermissionsMixin):
