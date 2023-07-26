739b0ad66850b22270ab3d46d704f0941c65789c
anikachurilova <nyuta.churilova@gmail.com>
2023-07-26 13:54:40 +0200

    access request: add secret link expiration access request setting
    * closes https://github.com/inveniosoftware/invenio-rdm-records/issues/1387

--- invenio_rdm_records/records/systemfields/access/access_settings.py
+++ invenio_rdm_records/records/systemfields/access/access_settings.py
@@ -17,6 +17,7 @@ class AccessSettings:
         self.allow_user_requests = settings_dict.get("allow_user_requests", False)
         self.allow_guest_requests = settings_dict.get("allow_guest_requests", False)
         self.accept_conditions_text = settings_dict.get("accept_conditions_text", None)
+        self.secret_link_expiration = settings_dict.get("secret_link_expiration", None)
 
     def dump(self):
         """Dump the record as dictionary."""
@@ -24,13 +25,15 @@ class AccessSettings:
             "allow_user_requests": self.allow_user_requests,
             "allow_guest_requests": self.allow_guest_requests,
             "accept_conditions_text": self.accept_conditions_text,
+            "secret_link_expiration": self.secret_link_expiration,
         }
 
     def __repr__(self):
         """Return repr(self)."""
-        return "<{} requests: {}/{}, text: {}>".format(
+        return "<{} requests: {}/{}, text: {}, secret link expiration: {}>".format(
             type(self).__name__,
             self.allow_guest_requests,
             self.allow_user_requests,
             self.accept_conditions_text,
+            self.secret_link_expiration,
         )
