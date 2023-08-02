9da6f248f0867ce203d22a7bbaf9aae766cf9503
alejandromumo <manuel.alejandro.de.oliveira.da.costa@cern.ch>
2023-08-02 13:46:06 +0200

    moderation: added empty actions

---
+++ invenio_rdm_records/requests/user_moderation/__init__.py
@@ -0,0 +1,11 @@
+# -*- coding: utf-8 -*-
+#
+# Copyright (C) 2023 CERN.
+#
+# Invenio-RDM is free software; you can redistribute it and/or modify
+# it under the terms of the MIT License; see LICENSE file for more details.
+"""User moderation actions specific to RDM-Records."""
+
+from .actions import on_block, on_restore
+
+__all__ = ("on_block", "on_restore")
