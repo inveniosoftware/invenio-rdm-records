34b33eee089bfde1b5d6883ec5169c51c5e10936
Pablo Saiz <pablo.saiz@cern.ch>
2023-08-29 15:22:07 +0200

    stats: Reindex documents after the stats have been calculated

--- invenio_rdm_records/services/tasks.py
+++ invenio_rdm_records/services/tasks.py
@@ -10,6 +10,9 @@
 from celery import shared_task
 from flask import current_app
 from invenio_access.permissions import system_identity
+from invenio_search.engine import dsl
+from invenio_search.proxies import current_search_client
+from invenio_search.utils import prefix_index
 
 from ..proxies import current_rdm_records
 from .errors import EmbargoNotLiftedError
@@ -27,3 +30,40 @@ def update_expired_embargos():
         except EmbargoNotLiftedError as ex:
             current_app.logger.warning(ex.description)
             continue
+
+
+@shared_task(ignore_result=True)
+def reindex_stats():
+    """Reindex the documents where the stats have changed."""
+    buffer_index = prefix_index("buffer_stats_to_reindex")
+    client = current_search_client
+    if not dsl.Index(buffer_index, using=client).exists():
+        return
+    documents = (
+        dsl.Search(
+            using=client,
+            index=buffer_index,
+        )
+        .source(includes=["timestamp"])
+        .execute()
+    )
+    latest = None
+    all_parents = []
+    for doc in documents:
+        all_parents.append(doc.meta["id"])
+        if not latest or latest < doc.get("timestamp"):
+            latest = doc.get("timestamp")
+    if not all_parents:
+        return
+    records_q = dsl.Q("terms", parent__id=all_parents)
+
+    current_rdm_records.records_service.reindex(
+        params={"allversions": True},
+        identity=system_identity,
+        extra_filter=records_q,
+    )
+    if latest:
+        client.delete_by_query(
+            index=buffer_index,
+            body={"query": {"range": {"timestamp": {"lte": latest}}}},
+        )
