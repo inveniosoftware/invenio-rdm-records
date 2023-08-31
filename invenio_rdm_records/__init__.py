3fde8bd07f743ee6dc989efcccbf7be9aa05f53a
Maximilian Moser <maximilian.moser@tuwien.ac.at>
2023-08-31 16:37:44 +0200

    release: v1.3.6
    * pinning the 'babel' package because it causes the UI
      serialization tests to fail with unexpected unicode characters
      (u2009, thin space)

--- invenio_rdm_records/__init__.py
+++ invenio_rdm_records/__init__.py
@@ -2,6 +2,7 @@
 #
 # Copyright (C) 2019-2022 CERN.
 # Copyright (C) 2019-2022 Northwestern University.
+# Copyright (C) 2023      TU Wien.
 #
 # Invenio-RDM-Records is free software; you can redistribute it and/or modify
 # it under the terms of the MIT License; see LICENSE file for more details.
@@ -10,6 +11,6 @@
 
 from .ext import InvenioRDMRecords
 
-__version__ = "1.3.5"
+__version__ = "1.3.6"
 
 __all__ = ("__version__", "InvenioRDMRecords")
