4b4badaaf3bd1292b4f70a9bc2d9ecd4e86d9f28
Zacharias Zacharodimos <zacharias.zacharodimos@cern.ch>
2023-09-04 17:11:48 +0200

    dumper: add files dumper ext
    * The extension is ommiting any files information if the record
      is public with restricted files

---
+++ invenio_rdm_records/records/dumpers/files.py
@@ -0,0 +1,26 @@
+# -*- coding: utf-8 -*-
+#
+# Copyright (C) 2020 CERN.
+#
+# Invenio-RDM-Records is free software; you can redistribute it and/or modify
+# it under the terms of the MIT License; see LICENSE file for more details.
+
+"""Search dumpers for location information."""
+
+
+from invenio_records.dumpers import SearchDumperExt
+
+from invenio_rdm_records.records.systemfields.access.protection import Visibility
+
+
+class FilesDumperExt(SearchDumperExt):
+    """Files dumper."""
+
+    def dump(self, record, data):
+        """Dump the data."""
+        public_record_with_restricted_files = (
+            data["access"].get("record") == Visibility.PUBLIC.value
+            and data["access"].get("files") == Visibility.RESTRICTED.value
+        )
+        if public_record_with_restricted_files:
+            data["files"] = {"enabled": data["files"]["enabled"]}
