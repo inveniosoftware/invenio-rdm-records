9f6fc8b774477979b1437b38b963e8c246ae5656
yashlamba <yashlamba2000@gmail.com>
2023-12-07 10:54:26 +0100

    serializers: iiif: add check for no files

--- invenio_rdm_records/resources/serializers/iiif/schema.py
+++ invenio_rdm_records/resources/serializers/iiif/schema.py
@@ -218,12 +218,13 @@ class IIIFManifestV2Schema(Schema):
     @pre_dump
     def add_iiif_links(self, data, many):
         """Add IIIF links to each files entry."""
-        for file in data["files"]["entries"].values():
-            if "links" not in file:
-                links_tpl = current_rdm_records_service.files.file_links_item_tpl(
-                    data["id"]
-                )
-                obj = SimpleNamespace(**file)
-                links = links_tpl.expand(system_identity, obj)
-                file["links"] = links
+        if "entries" in data["files"]:
+            for file in data["files"]["entries"].values():
+                if "links" not in file:
+                    links_tpl = current_rdm_records_service.files.file_links_item_tpl(
+                        data["id"]
+                    )
+                    obj = SimpleNamespace(**file)
+                    links = links_tpl.expand(system_identity, obj)
+                    file["links"] = links
         return data
