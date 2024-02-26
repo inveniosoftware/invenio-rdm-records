754d7c45336c40a413844e0a3b3cd9ac0a9083be
alejandromumo <manuel.alejandro.de.oliveira.da.costa@cern.ch>
2024-02-26 17:11:57 +0100

    services: added bulk addition to record community
    * added bulk addition to record community service

---
+++ invenio_rdm_records/services/community_transfer/__init__.py
@@ -0,0 +1,12 @@
+# -*- coding: utf-8 -*-
+#
+# Copyright (C) 2024 CERN.
+#
+# Invenio-RDM-Records is free software; you can redistribute it and/or modify
+# it under the terms of the MIT License; see LICENSE file for more details.
+"""Community transfer service."""
+
+
+from .service import CommunityTransferService
+
+__all__ = ("CommunityTransferService",)
