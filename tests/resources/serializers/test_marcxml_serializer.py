dc0ffeeeb90b84011a872056e043311d4d595d73
Pablo Tamarit <pablo.tamarit@cern.ch>
2024-03-26 13:35:13 +0100

    tests: serializers: marcxml: do not remove whitespaces

--- tests/resources/serializers/test_marcxml_serializer.py
+++ tests/resources/serializers/test_marcxml_serializer.py
@@ -7,7 +7,6 @@
 
 """Resources serializers tests."""
 
-import re
 from io import BytesIO
 
 import pytest
@@ -71,72 +70,53 @@ def test_marcxml_serializer_minimal_record(running_app, minimal_record, parent):
 
     serialized_record = serializer.serialize_object(record.data)
 
-    expected_data = f"""
-        <?xmlversion='1.0'encoding='utf-8'?>
-        <record xmlns="http://www.loc.gov/MARC21/slim">
-            <leader>00000nam##2200000uu#4500</leader>
-            <controlfield tag="001">{record.id}</controlfield>
-            <datafield tag="024" ind1="" ind2="">
-                <subfieldcode="2">doi
-                </subfield>
-                <subfieldcode="a">10.1234/{record.id}
-                </subfield>
-            </datafield>
-            <datafield tag="909" ind1="C" ind2="O">
-                <subfieldcode="o">oai:inveniordm:{record.id}
-                </subfield>
-            </datafield>
-            <datafield tag="700" ind1="" ind2="">
-                <subfieldcode="a">TroyInc.
-                </subfield>
-            </datafield>
-            <datafield tag="245" ind1="" ind2="">
-                <subfieldcode="a">ARomansstory
-                </subfield>
-            </datafield>
-            <datafield tag="100" ind1="" ind2="">
-                <subfieldcode="a">Brown,Troy
-                </subfield>
-            </datafield>
-            <datafield tag="540" ind1="" ind2="">
-                <subfieldcode="a">info:eu-repo/semantics/metadata-onlyAccess
-                </subfield>
-            </datafield>
-            <datafield tag="260" ind1="" ind2="">
-                <subfieldcode="b">AcmeInc
-                </subfield>
-                <subfieldcode="c">2020-06-01
-                </subfield>
-            </datafield>
-            <datafield tag="901" ind1="" ind2="">
-                <subfieldcode="u">info:eu-repo/semantic/other
-                </subfield>
-            </datafield>
-            <datafield tag="024" ind1="" ind2="1">
-                <subfieldcode="a">{record.data["parent"]["id"]}
-                </subfield>
-            </datafield>
-            <controlfield tag="005">{str(parse(record["updated"]).timestamp())}</controlfield>
-            <datafield tag="542" ind1="" ind2="">
-                <subfieldcode="a">public
-                </subfield>
-            </datafield>
-            <datafield tag="773" ind1="" ind2="">
-                <subfieldcode="a">10.1234/{record.data["parent"]["id"]}
-                </subfield>
-                <subfieldcode="i">isVersionOf
-                </subfield>
-                <subfieldcode="n">doi
-                </subfield>
-            </datafield>
-        </record>
-    """
+    expected_data = f"""\
+<?xml version='1.0' encoding='utf-8'?>
+<record xmlns="http://www.loc.gov/MARC21/slim">
+  <leader>00000nam##2200000uu#4500</leader>
+  <controlfield tag="001">{record.id}</controlfield>
+  <datafield tag="024" ind1=" " ind2=" ">
+    <subfield code="2">doi</subfield>
+    <subfield code="a">10.1234/{record.id}</subfield>
+  </datafield>
+  <datafield tag="909" ind1="C" ind2="O">
+    <subfield code="o">oai:inveniordm:{record.id}</subfield>
+  </datafield>
+  <datafield tag="700" ind1=" " ind2=" ">
+    <subfield code="a">Troy Inc.</subfield>
+  </datafield>
+  <datafield tag="245" ind1=" " ind2=" ">
+    <subfield code="a">A Romans story</subfield>
+  </datafield>
+  <datafield tag="100" ind1=" " ind2=" ">
+    <subfield code="a">Brown, Troy</subfield>
+  </datafield>
+  <datafield tag="540" ind1=" " ind2=" ">
+    <subfield code="a">info:eu-repo/semantics/metadata-onlyAccess</subfield>
+  </datafield>
+  <datafield tag="260" ind1=" " ind2=" ">
+    <subfield code="b">Acme Inc</subfield>
+    <subfield code="c">2020-06-01</subfield>
+  </datafield>
+  <datafield tag="901" ind1=" " ind2=" ">
+    <subfield code="u">info:eu-repo/semantic/other</subfield>
+  </datafield>
+  <datafield tag="024" ind1=" " ind2="1">
+    <subfield code="a">{record.data["parent"]["id"]}</subfield>
+  </datafield>
+  <controlfield tag="005">{str(parse(record["updated"]).timestamp())}</controlfield>
+  <datafield tag="542" ind1=" " ind2=" ">
+    <subfield code="a">public</subfield>
+  </datafield>
+  <datafield tag="773" ind1=" " ind2=" ">
+    <subfield code="a">10.1234/{record.data["parent"]["id"]}</subfield>
+    <subfield code="i">isVersionOf</subfield>
+    <subfield code="n">doi</subfield>
+  </datafield>
+</record>
+"""
 
-    # Remove special characters and compare.
-    # ``\s`` matches whitespaces, newlines, tabs, etc
-    sanitized_data = re.sub("\s+", "", expected_data)
-    sanitized_record = re.sub("\s+", "", serialized_record)
-    assert sanitized_data == sanitized_record
+    assert serialized_record == expected_data
 
 
 def _add_file_to_record(recid, client, headers):
@@ -208,118 +188,114 @@ def test_marcxml_serializer_full_record(
     record.data["parent"]["communities"]["ids"] = [community.id, community2.id]
     serialized_record = serializer.serialize_object(record.data)
 
-    expected_data = f"""
-        <?xml version='1.0' encoding='utf-8'?>
-        <record xmlns="http://www.loc.gov/MARC21/slim">
-            <leader>00000nam##2200000uu#4500</leader>
-            <controlfield tag="001">{recid}</controlfield>
-            <datafield tag="024" ind1=" " ind2=" ">
-                <subfield code="2">doi</subfield>
-                <subfield code="a">10.1234/inveniordm.1234</subfield>
-            </datafield>
-            <datafield tag="909" ind1="C" ind2="O">
-                <subfield code="o">oai:vvv.com:abcde-fghij</subfield>
-                <subfield code="p">user-blr</subfield>
-                <subfield code="p">user-rdm</subfield>
-            </datafield>
-            <datafield tag="700" ind1=" " ind2=" ">
-                <subfield code="a">Full Name, Test</subfield>
-                <subfield code="u">CERN</subfield>
-            </datafield>
-            <datafield tag="700" ind1=" " ind2=" ">
-                <subfield code="a">Doe, John the Contributor</subfield>
-                <subfield code="u">CERN</subfield>
-            </datafield>
-            <datafield tag="700" ind1=" " ind2=" ">
-                <subfield code="a">Bar, Foo the Creator</subfield>
-                <subfield code="u">CERN</subfield>
-            </datafield>
-            <datafield tag="245" ind1=" " ind2=" ">
-                <subfield code="a">InvenioRDM</subfield>
-            </datafield>
-            <datafield tag="100" ind1=" " ind2=" ">
-                <subfield code="a">Nielsen, Lars Holm</subfield>
-                <subfield code="u">CERN</subfield>
-            </datafield>
-            <datafield tag="856" ind1=" " ind2="2">
-                <subfield code="a">doi:10.1234/foo.bar</subfield>
-            </datafield>
-            <datafield tag="540" ind1=" " ind2=" ">
-                <subfield code="a">info:eu-repo/semantics/embargoedAccess</subfield>
-            </datafield>
-            <datafield tag="540" ind1=" " ind2=" ">
-                <subfield code="a">A custom license</subfield>
-                <subfield code="u">https://customlicense.org/licenses/by/4.0/</subfield>
-            </datafield>
-            <datafield tag="540" ind1=" " ind2=" ">
-                <subfield code="a">Creative Commons Attribution 4.0 International</subfield>
-                <subfield code="u">https://creativecommons.org/licenses/by/4.0/legalcode</subfield>
-            </datafield>
-            <datafield tag="653" ind1=" " ind2=" ">
-                <subfield code="a">Abdominal Injuries</subfield>
-            </datafield>
-            <datafield tag="653" ind1=" " ind2=" ">
-                <subfield code="a">custom</subfield>
-            </datafield>
-            <datafield tag="520" ind1=" " ind2=" ">
-                <subfield code="a">&amp;lt;h1&amp;gt;Adescription&amp;lt;/h1&amp;gt;&amp;lt;p&amp;gt;withHTMLtags&amp;lt;/p&amp;gt;</subfield>
-            </datafield>
-            <datafield tag="520" ind1=" " ind2=" ">
-                <subfield code="a">Bla bla bla</subfield>
-            </datafield>
-            <datafield tag="260" ind1=" " ind2=" ">
-                <subfield code="b">InvenioRDM</subfield>
-                <subfield code="c">2018/2020-09</subfield>
-                <subfield code="c">info:eu-repo/date/embargoEnd/2131-01-01</subfield>
-            </datafield>
-            <datafield tag="901" ind1=" " ind2=" ">
-                <subfield code="u">info:eu-repo/semantic/other</subfield>
-            </datafield>
-            <datafield tag="520" ind1=" " ind2="1">
-                <subfield code="a">application/pdf</subfield>
-            </datafield>
-            <datafield tag="024" ind1=" " ind2="1">
-                <subfield code="a">{record["parent"]["id"]}</subfield>
-            </datafield>
-            <datafield tag="980" ind1=" " ind2=" ">
-                <subfield code="a">user-blr</subfield>
-            </datafield>
-            <datafield tag="980" ind1=" " ind2=" ">
-                <subfield code="a">user-rdm</subfield>
-            </datafield>
-            <datafield tag="520" ind1=" " ind2="2">
-                <subfield code="a">11 pages</subfield>
-            </datafield>
-            <datafield tag="024" ind1=" " ind2="3">
-                <subfield code="a">v1.0</subfield>
-            </datafield>
-            <datafield tag="856" ind1=" " ind2="1">
-                <subfield code="a">award_title=PersonalisedTreatmentForCysticFibrosisPatientsWithUltra-rareCFTRMutations(andbeyond);award_number=755021;award_identifiers_scheme=url;award_identifiers_identifier=https://cordis.europa.eu/project/id/755021;funder_id=00k4n6c32;funder_name=EuropeanCommission; </subfield>
-            </datafield>
-            <controlfield tag="005">{str(parse(record["updated"]).timestamp())}</controlfield>
-            <datafield tag="856" ind1="4" ind2=" ">
-                <subfield code="s">8</subfield>
-                <subfield code="z">md5:8bc944dbd052ef51652e70a5104492e3</subfield>
-                <subfield code="u">https://127.0.0.1:5000/records/{recid}/files/test.pdf</subfield>
-            </datafield>
-            <datafield tag="542" ind1=" " ind2=" ">
-                <subfield code="a">public</subfield>
-            </datafield>
-            <datafield tag="773" ind1=" " ind2=" ">
-                <subfield code="a">10.1234/foo.bar</subfield>
-                <subfield code="i">Is cited by</subfield>
-                <subfield code="n">doi</subfield>
-            </datafield>
-            <datafield tag="773" ind1=" " ind2=" ">
-                <subfield code="a">10.1234/{record["parent"]["id"]}</subfield>
-                <subfield code="i">isVersionOf</subfield>
-                <subfield code="n">doi</subfield>
-            </datafield>
-        </record>
-        """
+    expected_data = f"""\
+<?xml version='1.0' encoding='utf-8'?>
+<record xmlns="http://www.loc.gov/MARC21/slim">
+  <leader>00000nam##2200000uu#4500</leader>
+  <controlfield tag="001">{recid}</controlfield>
+  <datafield tag="024" ind1=" " ind2=" ">
+    <subfield code="2">doi</subfield>
+    <subfield code="a">10.1234/inveniordm.1234</subfield>
+  </datafield>
+  <datafield tag="909" ind1="C" ind2="O">
+    <subfield code="o">oai:vvv.com:abcde-fghij</subfield>
+    <subfield code="p">user-blr</subfield>
+    <subfield code="p">user-rdm</subfield>
+  </datafield>
+  <datafield tag="700" ind1=" " ind2=" ">
+    <subfield code="a">Full Name, Test</subfield>
+    <subfield code="u">CERN</subfield>
+  </datafield>
+  <datafield tag="700" ind1=" " ind2=" ">
+    <subfield code="a">Doe, John the Contributor</subfield>
+    <subfield code="u">CERN</subfield>
+  </datafield>
+  <datafield tag="700" ind1=" " ind2=" ">
+    <subfield code="a">Bar, Foo the Creator</subfield>
+    <subfield code="u">CERN</subfield>
+  </datafield>
+  <datafield tag="245" ind1=" " ind2=" ">
+    <subfield code="a">InvenioRDM</subfield>
+  </datafield>
+  <datafield tag="100" ind1=" " ind2=" ">
+    <subfield code="a">Nielsen, Lars Holm</subfield>
+    <subfield code="u">CERN</subfield>
+  </datafield>
+  <datafield tag="856" ind1=" " ind2="2">
+    <subfield code="a">doi:10.1234/foo.bar</subfield>
+  </datafield>
+  <datafield tag="540" ind1=" " ind2=" ">
+    <subfield code="a">info:eu-repo/semantics/embargoedAccess</subfield>
+  </datafield>
+  <datafield tag="540" ind1=" " ind2=" ">
+    <subfield code="a">A custom license</subfield>
+    <subfield code="u">https://customlicense.org/licenses/by/4.0/</subfield>
+  </datafield>
+  <datafield tag="540" ind1=" " ind2=" ">
+    <subfield code="a">Creative Commons Attribution 4.0 International</subfield>
+    <subfield code="u">https://creativecommons.org/licenses/by/4.0/legalcode</subfield>
+  </datafield>
+  <datafield tag="653" ind1=" " ind2=" ">
+    <subfield code="a">Abdominal Injuries</subfield>
+  </datafield>
+  <datafield tag="653" ind1=" " ind2=" ">
+    <subfield code="a">custom</subfield>
+  </datafield>
+  <datafield tag="520" ind1=" " ind2=" ">
+    <subfield code="a">&amp;lt;h1&amp;gt;A description&amp;lt;/h1&amp;gt; &amp;lt;p&amp;gt;with HTML tags&amp;lt;/p&amp;gt;</subfield>
+  </datafield>
+  <datafield tag="520" ind1=" " ind2=" ">
+    <subfield code="a">Bla bla bla</subfield>
+  </datafield>
+  <datafield tag="260" ind1=" " ind2=" ">
+    <subfield code="b">InvenioRDM</subfield>
+    <subfield code="c">2018/2020-09</subfield>
+    <subfield code="c">info:eu-repo/date/embargoEnd/2131-01-01</subfield>
+  </datafield>
+  <datafield tag="901" ind1=" " ind2=" ">
+    <subfield code="u">info:eu-repo/semantic/other</subfield>
+  </datafield>
+  <datafield tag="520" ind1=" " ind2="1">
+    <subfield code="a">application/pdf</subfield>
+  </datafield>
+  <datafield tag="024" ind1=" " ind2="1">
+    <subfield code="a">{record["parent"]["id"]}</subfield>
+  </datafield>
+  <datafield tag="980" ind1=" " ind2=" ">
+    <subfield code="a">user-blr</subfield>
+  </datafield>
+  <datafield tag="980" ind1=" " ind2=" ">
+    <subfield code="a">user-rdm</subfield>
+  </datafield>
+  <datafield tag="520" ind1=" " ind2="2">
+    <subfield code="a">11 pages</subfield>
+  </datafield>
+  <datafield tag="024" ind1=" " ind2="3">
+    <subfield code="a">v1.0</subfield>
+  </datafield>
+  <datafield tag="856" ind1=" " ind2="1">
+    <subfield code="a">award_title=Personalised Treatment For Cystic Fibrosis Patients With Ultra-rare CFTR Mutations (and beyond); award_number=755021; award_identifiers_scheme=url; award_identifiers_identifier=https://cordis.europa.eu/project/id/755021; funder_id=00k4n6c32; funder_name=European Commission; </subfield>
+  </datafield>
+  <controlfield tag="005">{str(parse(record["updated"]).timestamp())}</controlfield>
+  <datafield tag="856" ind1="4" ind2=" ">
+    <subfield code="s">8</subfield>
+    <subfield code="z">md5:8bc944dbd052ef51652e70a5104492e3</subfield>
+    <subfield code="u">https://127.0.0.1:5000/records/{recid}/files/test.pdf</subfield>
+  </datafield>
+  <datafield tag="542" ind1=" " ind2=" ">
+    <subfield code="a">public</subfield>
+  </datafield>
+  <datafield tag="773" ind1=" " ind2=" ">
+    <subfield code="a">10.1234/foo.bar</subfield>
+    <subfield code="i">Is cited by</subfield>
+    <subfield code="n">doi</subfield>
+  </datafield>
+  <datafield tag="773" ind1=" " ind2=" ">
+    <subfield code="a">10.1234/{record["parent"]["id"]}</subfield>
+    <subfield code="i">isVersionOf</subfield>
+    <subfield code="n">doi</subfield>
+  </datafield>
+</record>
+"""
 
-    # Remove special characters and compare.
-    # ``\s`` matches whitespaces, newlines, tabs, etc
-    sanitized_data = re.sub("\s+", "", expected_data)
-    sanitized_record = re.sub("\s+", "", serialized_record)
-    assert sanitized_data == sanitized_record
+    assert serialized_record == expected_data
