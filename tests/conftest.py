c6d883166ce3926119a39fac3c7b7a7c161eab4b
Carlin MacKenzie <carlin.mackenzie@gmail.com>
2024-04-04 15:12:48 +0200

    vocab: add marc to roles.yaml

--- tests/conftest.py
+++ tests/conftest.py
@@ -1352,7 +1352,7 @@ def date_type_v(app, date_type):
         {
             "id": "other",
             "title": {"en": "Other"},
-            "props": {"datacite": "Other"},
+            "props": {"datacite": "Other", "marc": "oth"},
             "type": "datetypes",
         },
     )
@@ -1395,7 +1395,7 @@ def contributors_role_v(app, contributors_role_type):
         system_identity,
         {
             "id": "other",
-            "props": {"datacite": "Other"},
+            "props": {"datacite": "Other", "marc": "oth"},
             "title": {"en": "Other"},
             "type": "contributorsroles",
         },
