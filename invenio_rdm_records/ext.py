79365221ee520391c5260568750597bf33f6aee8
alejandromumo <manuel.alejandro.de.oliveira.da.costa@cern.ch>
2024-07-25 11:18:06 +0200

    services: added record extension registry

--- invenio_rdm_records/ext.py
+++ invenio_rdm_records/ext.py
@@ -102,6 +102,7 @@ class InvenioRDMRecords(object):
     def init_app(self, app):
         """Flask application initialization."""
         self.init_config(app)
+        self.init_record_service_registry(app)
         self.init_services(app)
         self.init_resource(app)
         app.extensions["invenio-rdm-records"] = self
@@ -309,6 +310,24 @@ class InvenioRDMRecords(object):
         for config_item in datacite_config_items:
             if config_item in app.config:
                 app.config[config_item] = str(app.config[config_item])
+
+    def init_record_service_registry(self, app):
+        # TODO load entry points
+        self.record_service_registry = {}
+        self._register_entry_point(
+            self.record_service_registry,
+            "invenio_rdm_records.services.record_service_registry",
+        )
+        # Discussed w/Alex
+        # Interface could be the function itself that modifies the record class and returns it
+
+    def _register_entry_point(self, registry, ep_name):
+        """Load entry points into the given registry."""
+        for ep in set(entry_points(group=ep_name)):
+            ext_name = ep.name
+            callback = ep.load()
+            assert callable(callback)
+            registry.setdefault(ext_name, callback)
 
 
 def finalize_app(app):
