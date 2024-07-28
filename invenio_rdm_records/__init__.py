d3388a447efc96d65b78484804b5a1c2ce0ee9ac
Christoph Ladurner <christoph.ladurner@tugraz.at>
2024-07-28 23:02:01 +0200

    release: v10.8.0

--- invenio_rdm_records/__init__.py
+++ invenio_rdm_records/__init__.py
@@ -2,6 +2,7 @@
 #
 # Copyright (C) 2019-2024 CERN.
 # Copyright (C) 2019-2022 Northwestern University.
+# Copyright (C) 2024 Graz University of Technology.
 #
 # Invenio-RDM-Records is free software; you can redistribute it and/or modify
 # it under the terms of the MIT License; see LICENSE file for more details.
@@ -10,6 +11,6 @@
 
 from .ext import InvenioRDMRecords
 
-__version__ = "10.7.1"
+__version__ = "10.8.0"
 
 __all__ = ("__version__", "InvenioRDMRecords")
