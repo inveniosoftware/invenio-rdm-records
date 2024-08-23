e39c1eb4e990332566274878b704e5e591d19de6
Karolina Przerwa <karolina.m.przerwa@gmail.com>
2024-08-23 17:51:21 +0200

    jobs: register embargos update

---
+++ invenio_rdm_records/jobs/jobs.py
@@ -0,0 +1,12 @@
+from invenio_jobs.jobs import RegisteredTask
+
+from invenio_rdm_records.services.tasks import update_expired_embargos
+
+update_expired_embargos_cls = RegisteredTask.factory(
+    arguments_schema=None,
+    job_cls_name="UpdateEmbargoesJob",
+    id_="update_expired_embargos",
+    task=update_expired_embargos,
+    description="Updates expired embargos",
+    title="Update expired embargoes"
+)
