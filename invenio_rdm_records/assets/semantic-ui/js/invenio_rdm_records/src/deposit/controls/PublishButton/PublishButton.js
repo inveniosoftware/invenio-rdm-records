bdcbcccc9837c7515ef698df55b25541255b3578
Nicola Tarocco <ntarocco@gmail.com>
2024-09-26 14:09:16 +0200

    Revert "deposit: provide permissions to publish button"
    This reverts commit 48add095a0ebfff8701e8e6f25e7e9dc9b9903c9.

--- invenio_rdm_records/assets/semantic-ui/js/invenio_rdm_records/src/deposit/controls/PublishButton/PublishButton.js
+++ invenio_rdm_records/assets/semantic-ui/js/invenio_rdm_records/src/deposit/controls/PublishButton/PublishButton.js
@@ -1,7 +1,6 @@
 // This file is part of Invenio-RDM-Records
 // Copyright (C) 2020-2023 CERN.
 // Copyright (C) 2020-2022 Northwestern University.
-// Copyright (C) 2020-2022 Graz University of Technology.
 //
 // Invenio-RDM-Records is free software; you can redistribute it and/or modify it
 // under the terms of the MIT License; see LICENSE file for more details.
@@ -13,7 +12,7 @@ import _omit from "lodash/omit";
 import PropTypes from "prop-types";
 import React, { Component } from "react";
 import { connect } from "react-redux";
-import { Button, Icon, Message, Modal, Popup } from "semantic-ui-react";
+import { Button, Icon, Message, Modal } from "semantic-ui-react";
 import {
   DepositFormSubmitActions,
   DepositFormSubmitContext,
@@ -41,14 +40,10 @@ class PublishButtonComponent extends Component {
     this.closeConfirmModal();
   };
 
-  isDisabled = (values, isSubmitting, numberOfFiles, permissions) => {
+  isDisabled = (values, isSubmitting, numberOfFiles) => {
     const filesEnabled = _get(values, "files.enabled", false);
     const filesMissing = filesEnabled && !numberOfFiles;
-    return isSubmitting || filesMissing || !this.hasPermissions(permissions);
-  };
-
-  hasPermissions = (permissions) => {
-    return permissions.can_publish;
+    return isSubmitting || filesMissing;
   };
 
   render() {
@@ -59,44 +54,26 @@ class PublishButtonComponent extends Component {
       publishWithoutCommunity,
       formik,
       publishModalExtraContent,
-      permissions,
       ...ui
     } = this.props;
     const { isConfirmModalOpen } = this.state;
     const { values, isSubmitting, handleSubmit } = formik;
 
     const uiProps = _omit(ui, ["dispatch"]);
-    const publishDisabled = this.isDisabled(
-      values,
-      isSubmitting,
-      numberOfFiles,
-      permissions
-    );
 
-    const popupText = !this.hasPermissions(permissions)
-      ? i18next.t("You don't have permission to publish")
-      : i18next.t("Required fields are missing.");
     return (
       <>
-        <Popup
-          disabled={!publishDisabled}
-          content={popupText}
-          trigger={
-            <span>
-              <Button
-                disabled={publishDisabled}
-                name="publish"
-                onClick={this.openConfirmModal}
-                positive
-                icon="upload"
-                loading={isSubmitting && actionState === DRAFT_PUBLISH_STARTED}
-                labelPosition="left"
-                content={buttonLabel}
-                {...uiProps}
-                type="button" // needed so the formik form doesn't handle it as submit button i.e enable HTML validation on required input fields
-              />
-            </span>
-          }
+        <Button
+          disabled={this.isDisabled(values, isSubmitting, numberOfFiles)}
+          name="publish"
+          onClick={this.openConfirmModal}
+          positive
+          icon="upload"
+          loading={isSubmitting && actionState === DRAFT_PUBLISH_STARTED}
+          labelPosition="left"
+          content={buttonLabel}
+          {...uiProps}
+          type="button" // needed so the formik form doesn't handle it as submit button i.e enable HTML validation on required input fields
         />
         {isConfirmModalOpen && (
           <Modal
@@ -149,7 +126,6 @@ PublishButtonComponent.propTypes = {
   numberOfFiles: PropTypes.number.isRequired,
   formik: PropTypes.object.isRequired,
   publishModalExtraContent: PropTypes.string,
-  permissions: PropTypes.object.isRequired,
 };
 
 PublishButtonComponent.defaultProps = {
@@ -163,7 +139,6 @@ const mapStateToProps = (state) => ({
   actionState: state.deposit.actionState,
   numberOfFiles: Object.values(state.files.entries).length,
   publishModalExtraContent: state.deposit.config.publish_modal_extra,
-  permissions: state.deposit.permissions,
 });
 
 export const PublishButton = connect(
