ec1305fd1951f99ccc97c6537696a122d51c2a50
Fatimah Zulfiqar <fatimah0zulfiqar@gmail.com>
2024-10-01 10:49:17 +0200

    dumper: refactor and updated docstring

--- invenio_rdm_records/records/dumpers/subject_hierarchy.py
+++ invenio_rdm_records/records/dumpers/subject_hierarchy.py
@@ -18,6 +18,40 @@ class SubjectHierarchyDumperExt(SearchDumperExt):
 
     This dumper needs to be placed after the RelationDumper for subjects as it relies
     on dereferenced subjects with scheme, subject, and props.
+
+    Example
+    "subjects" :
+        [
+            {
+            "id" : "euroscivoc:425",
+            "subject" : "Energy and fuels",
+            "scheme" : "EuroSciVoc",
+            "props" : {
+                "parents" : "euroscivoc:25,euroscivoc:67",
+            },
+            "@v" : "ef9f1c4c-b469-4645-a4b2-0db9b1c42096::1"
+            }
+        ]
+
+    The above subject is dumped with hierarchy field and is transformed to
+
+    "subjects" :
+        [
+            {
+            "id" : "euroscivoc:425",
+            "subject" : "Energy and fuels",
+            "scheme" : "EuroSciVoc",
+            "props" : {
+                "parents" : "euroscivoc:25,euroscivoc:67",
+                "hierarchy" : [
+                    "euroscivoc:25",
+                    "euroscivoc:25,euroscivoc:67",
+                    "euroscivoc:25,euroscivoc:67,euroscivoc:425"
+                ]
+            },
+            "@v" : "ef9f1c4c-b469-4645-a4b2-0db9b1c42096::1"
+            }
+        ]
     """
 
     def __init__(self, splitchar=","):
@@ -57,7 +91,8 @@ class SubjectHierarchyDumperExt(SearchDumperExt):
                     subject_hierarchy = build_hierarchy(parents, current_subject_id)
                     subject.setdefault("props", {})["hierarchy"] = subject_hierarchy
 
-        data["metadata"] = data.get("metadata", {})
+        if awards:
+            data["metadata"]["funding"] = awards
 
     def load(self, data, record_cls):
         """Load the data from secondary storage (OpenSearch-like).
