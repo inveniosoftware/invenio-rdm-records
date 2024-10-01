88af7fff8465d789658f418dfa14ed9e0ebc4fee
Sam Arbid <samaa2@kth.se>
2024-10-01 20:32:34 +0200

    i18n: Improve handling of file existence check in compileCatalog
    *  Add a check for the existence of the file before attempting to read it
    *  Log an error message if the file does not exist
    * Improve readability by extracting the file existence check into a function

--- invenio_rdm_records/assets/semantic-ui/translations/invenio_rdm_records/scripts/compileCatalog.js
+++ invenio_rdm_records/assets/semantic-ui/translations/invenio_rdm_records/scripts/compileCatalog.js
@@ -1,10 +1,11 @@
 // This file is part of InvenioRdmRecords
 // Copyright (C) 2022 CERN.
+// Copyright (C) 2024 KTH Royal Institute of Technology.
 //
 // Invenio RDM is free software; you can redistribute it and/or modify it
 // under the terms of the MIT License; see LICENSE file for more details.
 
-const { readFileSync, writeFileSync } = require("fs");
+const { readFileSync, writeFileSync, existsSync } = require("fs");
 const { gettextToI18next } = require("i18next-conv");
 
 const PACKAGE_JSON_BASE_PATH = "./";
@@ -13,7 +14,7 @@ const { languages } = require(`../package`).config;
 // it accepts the same options as the cli.
 // https://github.com/i18next/i18next-gettext-converter#options
 const options = {
-  /* you options here */
+  /* your options here */
 };
 
 function save(target) {
@@ -24,17 +25,31 @@ function save(target) {
 
 if ("lang" === process.argv[2]) {
   const lang = process.argv[3];
-  gettextToI18next(
-    lang,
-    readFileSync(`${PACKAGE_JSON_BASE_PATH}messages/${lang}/messages.po`),
-    options
-  ).then(save(`${PACKAGE_JSON_BASE_PATH}messages/${lang}/translations.json`));
+  const inputFilePath = `${PACKAGE_JSON_BASE_PATH}messages/${lang}/messages.po`;
+  const outputFilePath = `${PACKAGE_JSON_BASE_PATH}messages/${lang}/translations.json`;
+
+  if (existsSync(inputFilePath)) {
+    gettextToI18next(lang, readFileSync(inputFilePath), options)
+      .then(save(outputFilePath))
+      .catch((err) => {
+        console.error(`Error converting ${inputFilePath}:`, err);
+      });
+  } else {
+    console.error(`File not found: ${inputFilePath} for language ${lang}`);
+  }
 } else {
   for (const lang of languages) {
-    gettextToI18next(
-      lang,
-      readFileSync(`${PACKAGE_JSON_BASE_PATH}messages/${lang}/messages.po`),
-      options
-    ).then(save(`${PACKAGE_JSON_BASE_PATH}messages/${lang}/translations.json`));
+    const inputFilePath = `${PACKAGE_JSON_BASE_PATH}messages/${lang}/messages.po`;
+    const outputFilePath = `${PACKAGE_JSON_BASE_PATH}messages/${lang}/translations.json`;
+
+    if (existsSync(inputFilePath)) {
+      gettextToI18next(lang, readFileSync(inputFilePath), options)
+        .then(save(outputFilePath))
+        .catch((err) => {
+          console.error(`Error converting ${inputFilePath}:`, err);
+        });
+    } else {
+      console.error(`File not found: ${inputFilePath} for language ${lang}`);
+    }
   }
 }
