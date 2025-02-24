3b37c5aafaa45846867278d5e1266a015f325e2d
Pablo Tamarit <pablo.tamarit@cern.ch>
2025-02-24 16:09:32 +0100

    views: signposting: files: fix filename encoding issues for downloads

--- tests/resources/serializers/test_signposting_serializer.py
+++ tests/resources/serializers/test_signposting_serializer.py
@@ -8,13 +8,38 @@
 
 """Resources serializers tests."""
 
+import pytest
+
 from invenio_rdm_records.resources.serializers import (
     FAIRSignpostingProfileLvl1Serializer,
     FAIRSignpostingProfileLvl2Serializer,
 )
 
 
-def test_signposting_serializer_full(running_app, full_record_to_dict):
+@pytest.fixture
+def full_record_to_dict_signposting(full_record_to_dict):
+    # raise ValueError("boom boom")
+    full_record_to_dict["files"] = {
+        "count": 1,
+        "enabled": True,
+        "entries": {
+            "testé.txt": {
+                "checksum": "md5:e795abeef2c38de2b064be9f6364ceae",
+                "ext": "txt",
+                "id": "d22bde05-5a36-48a3-86a7-acf2c4bb6f64",
+                "key": "testé.txt",
+                "metadata": None,
+                "mimetype": "text/plain",
+                "size": 9,
+            },
+        },
+        "order": [],
+        "total_bytes": 9,
+    }
+    return full_record_to_dict
+
+
+def test_signposting_serializer_full(running_app, full_record_to_dict_signposting):
     expected = {
         "linkset": [
             # Landing page Link Context Object
@@ -82,7 +107,7 @@ def test_signposting_serializer_full(running_app, full_record_to_dict):
                 ],
                 "item": [
                     {
-                        "href": "https://127.0.0.1:5000/records/12345-abcde/files/test.txt",  # noqa
+                        "href": "https://127.0.0.1:5000/records/12345-abcde/files/test%C3%A9.txt",  # noqa
                         "type": "text/plain",
                     }
                 ],
@@ -97,7 +122,7 @@ def test_signposting_serializer_full(running_app, full_record_to_dict):
             },
             # Content Resource (file) Link Context Object
             {
-                "anchor": "https://127.0.0.1:5000/records/12345-abcde/files/test.txt",
+                "anchor": "https://127.0.0.1:5000/records/12345-abcde/files/test%C3%A9.txt",
                 "collection": [
                     {
                         "href": "https://127.0.0.1:5000/records/12345-abcde",
@@ -118,15 +143,17 @@ def test_signposting_serializer_full(running_app, full_record_to_dict):
         ]
     }
 
-    serialized = FAIRSignpostingProfileLvl2Serializer().dump_obj(full_record_to_dict)
+    serialized = FAIRSignpostingProfileLvl2Serializer().dump_obj(
+        full_record_to_dict_signposting
+    )
 
     assert expected == serialized
 
 
-def test_signposting_lvl1_serializer_full(running_app, full_record_to_dict):
+def test_signposting_lvl1_serializer_full(running_app, full_record_to_dict_signposting):
     ui_url = "https://127.0.0.1:5000/records/12345-abcde"
     api_url = "https://127.0.0.1:5000/api/records/12345-abcde"
-    filename = "test.txt"
+    filename = "test%C3%A9.txt"
 
     expected = [
         f'<https://orcid.org/0000-0001-8135-3489> ; rel="author"',
@@ -154,7 +181,7 @@ def test_signposting_lvl1_serializer_full(running_app, full_record_to_dict):
     ]
 
     serialized = FAIRSignpostingProfileLvl1Serializer().serialize_object(
-        full_record_to_dict
+        full_record_to_dict_signposting
     )
 
     assert expected == serialized.split(" , ")
