293d76040e8c639507b00c446a88c4152bc179d3
Sam Arbid <samaa2@kth.se>
2025-03-19 15:33:40 +0100

    i18n: UI Support for Swedish language

--- invenio_rdm_records/assets/semantic-ui/translations/invenio_rdm_records/messages/index.js
+++ invenio_rdm_records/assets/semantic-ui/translations/invenio_rdm_records/messages/index.js
@@ -1,11 +1,13 @@
 import TRANSLATE_EN from "./en/translations.json";
 import TRANSLATE_DE from "./de/translations.json";
 import TRANSLATE_EL from "./el/translations.json";
+import TRANSLATE_SV from "./sv/translations.json";
 import TRANSLATE_CS from "./cs/translations.json";
 
 export const translations = {
   el: { translation: TRANSLATE_EL },
   en: { translation: TRANSLATE_EN },
   de: { translation: TRANSLATE_DE },
+  sv: { translation: TRANSLATE_SV },
   cs: { translation: TRANSLATE_CS },
 };
