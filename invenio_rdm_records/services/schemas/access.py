3a364a241ac8a99a9e9e0e1c529748da321b1dd6
Christoph Ladurner <christoph.ladurner@tugraz.at>
2025-05-10 22:44:55 +0200

    fix: RemovedInMarshmallow4Warning
    * The `ordered` `class Meta` option is deprecated. Field order is
      already preserved by default. Set `Schema.dict_class` to OrderedDict
      to maintain the previous behavior.
    
    * multiple api changes in marshmallow

--- invenio_rdm_records/services/schemas/access.py
+++ invenio_rdm_records/services/schemas/access.py
@@ -3,7 +3,7 @@
 # Copyright (C) 2020-2021 CERN.
 # Copyright (C) 2020-2021 Northwestern University.
 # Copyright (C) 2021 TU Wien.
-# Copyright (C) 2021-2023 Graz University of Technology.
+# Copyright (C) 2021-2025 Graz University of Technology.
 #
 # Invenio-RDM-Records is free software; you can redistribute it and/or modify
 # it under the terms of the MIT License; see LICENSE file for more details.
@@ -77,11 +77,11 @@ class AccessSchema(Schema):
         return getattr(obj, key, default)
 
     @validates("record")
-    def validate_record_protection(self, value):
+    def validate_record_protection(self, value, data_key=None):
         """Validate the record protection value."""
         self.validate_protection_value(value, "record")
 
     @validates("files")
-    def validate_files_protection(self, value):
+    def validate_files_protection(self, value, data_key=None):
         """Validate the files protection value."""
         self.validate_protection_value(value, "files")
