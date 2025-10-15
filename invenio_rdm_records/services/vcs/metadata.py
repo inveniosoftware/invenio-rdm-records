25d439fdff8ec61b122c04cc1bd7017379557491
Pal Kerecsenyi <pal@p.kerecs.com>
2025-10-15 16:33:40 +0200

    WIP: rename directories

--- invenio_rdm_records/services/vcs/metadata.py
+++ invenio_rdm_records/services/vcs/metadata.py
@@ -1,10 +1,10 @@
 # -*- coding: utf-8 -*-
 #
-# Copyright (C) 2023-2024 CERN.
+# Copyright (C) 2023-2025 CERN.
 #
 # Invenio-RDM-Records is free software; you can redistribute it and/or modify
 # it under the terms of the MIT License; see LICENSE file for more details.
-"""RDM github release metadata."""
+"""RDM VCS release metadata."""
 
 from datetime import datetime, timezone
 from typing import TYPE_CHECKING
--- invenio_rdm_records/services/vcs/release.py
+++ invenio_rdm_records/services/vcs/release.py
@@ -1,11 +1,11 @@
 # -*- coding: utf-8 -*-
 #
-# Copyright (C) 2023-2024 CERN.
+# Copyright (C) 2023-2025 CERN.
 # Copyright (C) 2024 KTH Royal Institute of Technology.
 #
 # Invenio-RDM-Records is free software; you can redistribute it and/or modify
 # it under the terms of the MIT License; see LICENSE file for more details.
-"""Github release API implementation."""
+"""VCS release API implementation."""
 
 from flask import current_app
 from invenio_access.permissions import authenticated_user, system_identity
