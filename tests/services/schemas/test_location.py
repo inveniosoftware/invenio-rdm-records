51917fcd10f01138f727b51c744c43bf5e1224b6
Alex Ioannidis <a.ioannidis@cern.ch>
2026-01-07 15:47:03 +0100

    fix(tests): use valid WikiData IDs and Geonames in fixtures

--- tests/services/schemas/test_location.py
+++ tests/services/schemas/test_location.py
@@ -21,8 +21,8 @@ def valid_full_location():
         "place": "test location place",
         "description": "test location description",
         "identifiers": [
-            {"identifier": "12345abcde", "scheme": "wikidata"},
-            {"identifier": "12345abcde", "scheme": "geonames"},
+            {"identifier": "Q39", "scheme": "wikidata"},
+            {"identifier": "https://www.geonames.org/2660646", "scheme": "geonames"},
         ],
     }
 
