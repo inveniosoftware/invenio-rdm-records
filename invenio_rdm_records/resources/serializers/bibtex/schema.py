aab9360ce915c04a579bad96aa6b29c27f140b26
yashlamba <yashlamba2000@gmail.com>
2026-01-08 14:26:59 +0100

    bibtex: schema: add publication-thesis for compatibility

--- invenio_rdm_records/resources/serializers/bibtex/schema.py
+++ invenio_rdm_records/resources/serializers/bibtex/schema.py
@@ -1,6 +1,6 @@
 # -*- coding: utf-8 -*-
 #
-# Copyright (C) 2023-2025 CERN
+# Copyright (C) 2023-2026 CERN
 #
 # Invenio-RDM-Records is free software; you can redistribute it and/or modify
 # it under the terms of the MIT License; see LICENSE file for more details.
@@ -60,6 +60,7 @@ class BibTexSchema(BaseSerializerSchema, CommonFieldsMixin):
         ],
         "publication-article": [BibTexFormatter.article],
         "publication-preprint": [BibTexFormatter.unpublished],
+        "publication-thesis": [BibTexFormatter.thesis],
         "publication-dissertation": [
             BibTexFormatter.thesis
         ],  # Previously publication-thesis
