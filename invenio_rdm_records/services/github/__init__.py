65800628104d583abd61f9a64cf31a8ac35d81d2
Pal Kerecsenyi <pal@p.kerecs.com>
2026-01-09 12:02:51 +0100

    WIP: add back old github bindings, make VCS dependency optional

---
+++ invenio_rdm_records/services/github/__init__.py
@@ -0,0 +1,12 @@
+# -*- coding: utf-8 -*-
+#
+# Copyright (C) 2023-2025 CERN.
+#
+# Invenio-RDM-Records is free software; you can redistribute it and/or modify
+# it under the terms of the MIT License; see LICENSE file for more details.
+"""RDM records implementation of the legacy Invenio-GitHub module.
+
+This module is now deprecated. For now, to allow for an optional migration, the RDM bindings for
+Invenio-GitHub have been retained. Please migrate to Invenio-VCS.
+"""
+# TODO: add link to migration docs.
