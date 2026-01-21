6a3e7738fbc2aa9b7563c0d31de0a3f314cd3790
Tom Morrell <tmorrell@caltech.edu>
2026-01-21 13:44:01 -0800

    chore: black formatting python3.9 compatability

--- invenio_rdm_records/services/schemas/metadata.py
+++ invenio_rdm_records/services/schemas/metadata.py
@@ -335,6 +335,9 @@ class LocationSchema(Schema):
     @validates_schema
     def validate_data(self, data, **kwargs):
         """Validate identifier based on type."""
+        # Turning off formatting for Python 3.9 black compatability
+        # Remove these comments once Python 3.9 support is no longer needed
+        # fmt: off
         if (
             not data.get("geometry")
             and not data.get("place")
@@ -345,6 +348,7 @@ class LocationSchema(Schema):
                 {"locations": _("At least one of ['geometry', 'place', \
                 identifiers', 'description'] must be present.")}
             )
+        # fmt: on
 
 
 class FeatureSchema(Schema):
