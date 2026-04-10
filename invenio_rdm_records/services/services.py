352da83bb5a24c4a43b296ddae3a69a373c968cf
Maira Salazar <mpssalazar@gmail.com>
2026-04-10 10:59:17 +0200

    fix: always add policy_id for file modification

--- invenio_rdm_records/services/services.py
+++ invenio_rdm_records/services/services.py
@@ -324,8 +324,7 @@ class RDMRecordService(RecordService):
         if disabled or forbidden:  # bail early
             raise PermissionDeniedError()
 
-        if immediate_file_mod.policy:
-            data["policy_id"] = immediate_file_mod.policy.get("id")
+        data["policy_id"] = immediate_file_mod.policy.get("id")
 
         request = requests_service.create(
             identity,
