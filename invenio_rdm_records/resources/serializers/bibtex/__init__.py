789cf1617a87cd54ccef4328d4e60d18c50e8fe6
Maximilian Moser <maximilian.moser@tuwien.ac.at>
2026-06-29 22:30:26 +0200

    chore(format): reformat with black

--- invenio_rdm_records/resources/serializers/bibtex/__init__.py
+++ invenio_rdm_records/resources/serializers/bibtex/__init__.py
@@ -19,7 +19,7 @@ class BibtexSerializer(MarshmallowSerializer):
             object_schema_cls=BibTexSchema,
             list_schema_cls=BaseListSchema,
             encoder=self.bibtex_tostring,
-            **options
+            **options,
         )
 
     @classmethod
