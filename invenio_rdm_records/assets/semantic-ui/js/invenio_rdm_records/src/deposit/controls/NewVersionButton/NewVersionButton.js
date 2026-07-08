69166b71332063418e03d898124395c998d694f6
Pal Kerecsenyi <pal@p.kerecs.com>
2026-07-08 11:52:10 +0200

    feat(ui): make NewVersionButton overridable
    * Added a React Overridable wrapper to the new version button, along
    with show/hide support (as already applies to many fields on the deposit
    form)
    
    * The exported name of the component is unchanged and the props remain
    the same, so this change is fully backward-compatible.

--- invenio_rdm_records/assets/semantic-ui/js/invenio_rdm_records/src/deposit/controls/NewVersionButton/NewVersionButton.js
+++ invenio_rdm_records/assets/semantic-ui/js/invenio_rdm_records/src/deposit/controls/NewVersionButton/NewVersionButton.js
@@ -5,12 +5,13 @@
  */
 
 import React, { useState } from "react";
-import { http } from "react-invenio-forms";
+import { http, showHideOverridable } from "react-invenio-forms";
 import { Icon, Button, Popup } from "semantic-ui-react";
 import { i18next } from "@translations/invenio_rdm_records/i18next";
 import PropTypes from "prop-types";
+import Overridable from "react-overridable";
 
-export const NewVersionButton = ({ onError, record, disabled, ...uiProps }) => {
+const NewVersionButtonComponent = ({ onError, record, disabled, ...uiProps }) => {
   const [loading, setLoading] = useState(false);
 
   const handleClick = async () => {
@@ -27,39 +28,51 @@ export const NewVersionButton = ({ onError, record, disabled, ...uiProps }) => {
   };
 
   return (
-    <Popup
-      content={i18next.t("You don't have permissions to create a new version.")}
-      position="top center"
-      disabled={!disabled}
-      trigger={
-        // Extra span needed since disabled buttons do not trigger hover events
-        <span>
-          <Button
-            type="button"
-            positive
-            size="mini"
-            onClick={handleClick}
-            loading={loading}
-            icon
-            labelPosition="left"
-            disabled={disabled}
-            {...uiProps}
-          >
-            <Icon name="tag" />
-            {i18next.t("New version")}
-          </Button>
-        </span>
-      }
-    />
+    <Overridable
+      id="InvenioRdmRecords.RecordLandingPage.RecordManagement.NewVersionButton.container"
+      onError={onError}
+      record={record}
+      disabled={disabled}
+    >
+      <Popup
+        content={i18next.t("You don't have permissions to create a new version.")}
+        position="top center"
+        disabled={!disabled}
+        trigger={
+          // Extra span needed since disabled buttons do not trigger hover events
+          <span>
+            <Button
+              type="button"
+              positive
+              size="mini"
+              onClick={handleClick}
+              loading={loading}
+              icon
+              labelPosition="left"
+              disabled={disabled}
+              {...uiProps}
+            >
+              <Icon name="tag" />
+              {i18next.t("New version")}
+            </Button>
+          </span>
+        }
+      />
+    </Overridable>
   );
 };
 
-NewVersionButton.propTypes = {
+NewVersionButtonComponent.propTypes = {
   onError: PropTypes.func.isRequired,
   record: PropTypes.object.isRequired,
   disabled: PropTypes.bool,
 };
 
-NewVersionButton.defaultProps = {
+NewVersionButtonComponent.defaultProps = {
   disabled: false,
 };
+
+export const NewVersionButton = showHideOverridable(
+  "InvenioRdmRecords.RecordLandingPage.RecordManagement.NewVersionButton",
+  NewVersionButtonComponent
+);
