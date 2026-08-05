18f8418d53352cb51e3bf0dfb10a3ded89e15735
Eduard Nitu <edi.nitu2000@yahoo.com>
2026-08-05 15:01:21 +0200

    feat: new ChecksRun API for RDMRecords
      * implement an API to search for all CheckRuns for a particular
        RDMRecord - at /api/<pid_value>/checks
      * this API is (for now) meant to be used only by other service
        calls, not by the UI

---
+++ invenio_rdm_records/services/checks/__init__.py
@@ -0,0 +1,8 @@
+# SPDX-FileCopyrightText: 2026 Graz University of Technology.
+# SPDX-License-Identifier: MIT
+
+"""Record checks service."""
+
+from .service import RDMRecordChecksService
+
+__all__ = ("RDMRecordChecksService",)
