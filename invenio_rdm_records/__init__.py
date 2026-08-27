5dfdac649c0d7b1de261f08571b37743fb04a2be
Sam Arbid <samaa2@kth.se>
2026-08-27 11:19:27 +0200

    📦 release: v32.2.2

--- invenio_rdm_records/__init__.py
+++ invenio_rdm_records/__init__.py
@@ -1,6 +1,6 @@
 # SPDX-FileCopyrightText: 2019-2026 CERN.
 # SPDX-FileCopyrightText: 2019-2024 Northwestern University.
-# SPDX-FileCopyrightText: 2024 KTH Royal Institute of Technology.
+# SPDX-FileCopyrightText: 2024-2026 KTH Royal Institute of Technology.
 # SPDX-FileCopyrightText: 2024-2026 Graz University of Technology.
 # SPDX-FileCopyrightText: 2026 TU Wien.
 # SPDX-License-Identifier: MIT
@@ -9,6 +9,6 @@
 
 from .ext import InvenioRDMRecords
 
-__version__ = "32.2.1"
+__version__ = "32.2.2"
 
 __all__ = ("__version__", "InvenioRDMRecords")
